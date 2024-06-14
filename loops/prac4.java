3f2a4c932be07fa1519835732c6d040787a35766
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-14 22:48:28 +0530

    prac3 and prac4 added

--- loops/prac4.java
+++ loops/prac4.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 //Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
 
-public class prac3 {
+public class prac4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[10];
 
+        int sumAllElements = 0;
         // taking input of integers
         for (int i = 0; i < 10; i++) {
             System.out.print("Enter element : ");
             arr[i] = sc.nextInt();
+            sumAllElements += arr[i];
         }
 
         // displaying integers
@@ -21,15 +23,17 @@ public class prac3 {
         }
 
         int evenSum = 0;
-        int oddSum = 0;
+        
 
+        //sum of even
         for (int i = 0; i < 10; i++) {
             if (arr[i] % 2 == 0) {
                 evenSum += arr[i];
-            } else {
-                oddSum += arr[i];
             }
+           
         }
+        int oddSum = sumAllElements - evenSum;
+
         System.out.println();
 
         System.out.println("The sum of even integers is : " + evenSum);
@@ -40,3 +44,4 @@ public class prac3 {
     }
 
 }
+
