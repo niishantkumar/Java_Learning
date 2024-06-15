50535b2054101e64e3b224146311825e664606ec
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-15 23:51:42 +0530

    prac3.java modified

--- patternI/prac3.java
+++ patternI/prac3.java
@@ -9,7 +9,12 @@ package Java_Learning.patternI;
 
 public class prac3 {
     public static void main(String[] args) {
-        
+        for (byte i = 1; i <= 4; i++) {
+            for (byte j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
-    
+
 }
