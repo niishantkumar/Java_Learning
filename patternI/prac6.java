1bbfa8ba2945f0e082828a761171cf11eec47044
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-17 12:45:04 +0530

    prac6.java modified

---
+++ patternI/prac6.java
@@ -0,0 +1,26 @@
+package Java_Learning.patternI;
+/*
+ * * * * *
+ *       *
+ *       *
+ *       *
+ * * * * *
+ */
+
+public class prac6 {
+    public static void main(String[] args) {
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                if (row == 1 || col == 1 || row == 5 || col == 5) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+}
