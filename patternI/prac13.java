c0e679de15fa7957823e261c171f9747135920a4
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-18 00:04:46 +0530

    prac13.java modified

--- patternI/prac13.java
+++ patternI/prac13.java
@@ -1,4 +1,5 @@
 package Java_Learning.patternI;
+
 import java.util.Scanner;
 
 /*
@@ -11,7 +12,24 @@ import java.util.Scanner;
 
 public class prac13 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        // tatal number of lines
+        System.out.print("Enter number of lines : ");
+        int tLines = sc.nextInt();
+
+        // loop to print
+        for (int line = 1; line <= tLines; line++) {
+            // for space loop
+            for (int space = tLines; space > line; space--) {
+                System.out.print(" ");
+            }
+
+            // for digits
+            for (int digit = 1; digit <= line; digit++) {
+                System.out.print(line + " ");
+            }
+            System.out.println();
+        }
     }
-    
 }
