17490ef8bcf00147617f6e133b7785395b368977
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-17 23:46:55 +0530

    prac13.java added

---
+++ patternI/prac13.java
@@ -0,0 +1,17 @@
+package Java_Learning.patternI;
+import java.util.Scanner;
+
+/*
+    1
+   2 2
+  3 3 3
+ 4 4 4 4
+5 5 5 5 5
+*/
+
+public class prac13 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
