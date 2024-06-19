521c3827ba9088b0674897c0ff123c1453fb968d
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-19 17:47:15 +0530

    prac2.java added

---
+++ funcMethod/prac2.java
@@ -0,0 +1,10 @@
+package Java_Learning.funcMethod;
+import java.util.Scanner;
+
+//func to display sum of two integers passed as parameters
+
+public class prac2 {
+    public static void main(String[] args) {
+        
+    }
+}
