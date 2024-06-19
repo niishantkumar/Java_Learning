d858d45043d744aff718a87cc0ea77355c7fa86b
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-20 01:27:09 +0530

    prac2.java modifieded

--- funcMethod/prac2.java
+++ funcMethod/prac2.java
@@ -7,7 +7,7 @@ public class prac2 {
 
     public static void sum(int num1, int num2){
         int sum = num1+num2;
-        System.out.println("The sum is : "+sum);
+        System.out.print("The sum is : "+sum);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,7 +20,7 @@ public class prac2 {
 
         //calling func
         sum(a, b);
-        
+
         sc.close();
     }
 }
