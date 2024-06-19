b7a1c6d106871608644f7be83681db8dacadc3f6
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-19 17:51:40 +0530

    prac2.java modified

--- funcMethod/prac2.java
+++ funcMethod/prac2.java
@@ -4,7 +4,23 @@ import java.util.Scanner;
 //func to display sum of two integers passed as parameters
 
 public class prac2 {
+
+    public static void sum(int num1, int num2){
+        int sum = num1+num2;
+        System.out.println("The sum is : "+sum);
+    }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //input of integers for sum
+        System.out.print("Enter number : ");
+        int a = sc.nextInt();
+        System.out.print("Enter number : ");
+        int b = sc.nextInt();
+
+        //calling func
+        sum(a, b);
         
+        sc.close();
     }
 }
