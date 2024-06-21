4f4ccb63c3464ea99fc730b26a0a18b7fdba87fd
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-21 19:56:04 +0530

    prac7.java added

---
+++ funcMethod/prac7.java
@@ -0,0 +1,18 @@
+package Java_Learning.funcMethod;
+
+//to show func overloading using diff types of parameter
+
+public class prac7 {
+    public static void sum(int a, int b) {
+        System.out.println("The sum is : " + (a + b));
+    }
+
+    public static void sum(float a, float b) {
+        System.out.println("The sum is : " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        sum(10, 20);
+        sum(10.2f, 20f);
+    }
+}
