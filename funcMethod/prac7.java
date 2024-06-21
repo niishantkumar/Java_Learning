562af4f522316457613152a2156e802fb8047e6a
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-21 20:01:21 +0530

    prac7.java modified

--- funcMethod/prac7.java
+++ funcMethod/prac7.java
@@ -11,8 +11,13 @@ public class prac7 {
         System.out.println("The sum is : " + (a + b));
     }
 
+    public static void sum(float a, int b) {
+        System.out.println("The sum is : " + (a + b));
+    }
+
     public static void main(String[] args) {
         sum(10, 20);
         sum(10.2f, 20f);
+        sum(10.2f, 20);
     }
 }
