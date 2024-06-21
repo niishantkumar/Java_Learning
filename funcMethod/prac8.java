843223bd0132db30051ac34f3bc48b85f10324f5
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-21 20:14:02 +0530

    prac8.java modified

--- funcMethod/prac8.java
+++ funcMethod/prac8.java
@@ -3,5 +3,16 @@ package Java_Learning.funcMethod;
 //to show func overloading using diff types of parameter
 
 public class prac8 {
-    
+    public static void sum(int a, int b, int c) {
+        System.out.println("The sum is : " + (a + b + c));
+    }
+
+    public static void sum(int a, int b) {
+        System.out.println("The sum is : " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        sum(10, 20, 40);
+        sum(10, 20);
+    }
 }
