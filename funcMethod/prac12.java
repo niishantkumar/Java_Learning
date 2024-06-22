a8486cbe26c6f588ba7f91ac843ed117b6d9aab3
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-22 16:53:30 +0530

    prac12.java added

---
+++ funcMethod/prac12.java
@@ -0,0 +1,12 @@
+package Java_Learning.funcMethod;
+
+import java.util.Scanner;
+
+//to convert binary to decimal
+
+public class prac12 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
