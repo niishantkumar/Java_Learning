9efbf95f75231383c7e8fde123dc494ab8071f3d
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-22 17:06:17 +0530

    prac12.java modified

--- funcMethod/prac12.java
+++ funcMethod/prac12.java
@@ -2,11 +2,33 @@ package Java_Learning.funcMethod;
 
 import java.util.Scanner;
 
-//to convert binary to decimal
+//to calculate binary to decimal
 
 public class prac12 {
 
+    // func to calculate bin to decimal
+    public static int binToDeci(int num) {
+        int decimal = 0;
+        int power = 0;
+
+        while (num != 0) {
+            decimal += ((num % 10) * Math.pow(2, power));
+            num /= 10;
+            power++;
+        }
+
+        return decimal;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // taking input of bin number
+        System.out.print("Enter binary number : ");
+        int num = sc.nextInt();
+
+        System.out.println("Decimal number is : " + binToDeci(num));
+
+        sc.close();
     }
 }
