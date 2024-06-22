1d00e74d2b3f52293ee33c23f7984a0c6772c110
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-22 21:03:23 +0530

    prac1.java modified

--- funcMethod/pracQues/prac1.java
+++ funcMethod/pracQues/prac1.java
@@ -1,11 +1,25 @@
 package Java_Learning.funcMethod.pracQues;
+
 import java.util.Scanner;
 
 //Write a Java method to compute the average of three numbers
 
 public class prac1 {
-    public static void main(String[] args) {
-        
+    // average of three num
+    public static void avgThreeNum() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter numbers :");
+        float num1 = sc.nextInt();
+        float num2 = sc.nextInt();
+        float num3 = sc.nextInt();
+
+        sc.close();
+
+        System.out.println("Avg = " + ((num1 + num2 + num3) / 3));
     }
-    
+
+    public static void main(String[] args) {
+        avgThreeNum();
+    }
 }
