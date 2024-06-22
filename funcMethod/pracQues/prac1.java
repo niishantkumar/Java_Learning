227b0616ce49be14949d67633059e12bc15ee1ac
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-22 20:53:17 +0530

    prac1.java added

---
+++ funcMethod/pracQues/prac1.java
@@ -0,0 +1,11 @@
+package Java_Learning.funcMethod.pracQues;
+import java.util.Scanner;
+
+//Write a Java method to compute the average of three numbers
+
+public class prac1 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
