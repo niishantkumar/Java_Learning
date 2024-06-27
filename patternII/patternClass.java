cad8b211ea30a2fffa7bc1f6a9bf9d2fc2fb1886
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-27 18:03:18 +0530

    in patternClass.java rotHalfPyrNum() func added

--- patternII/patternClass.java
+++ patternII/patternClass.java
@@ -54,8 +54,21 @@ public class patternClass {
         }
     }
 
+    // to print inverted and rotated half pyramid with number
+    public static void rotHalfPyrNum() {
+        int totalLine = 5;
+        for (int i = 1; i <= totalLine; i++) {
+            // loop for numbers
+            for (int j = 1; j <= totalLine - i + 1; j++) {
+                
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // squarePat();
-        rotHalfPyr();
+        rotHalfPyrNum();
     }
 }
