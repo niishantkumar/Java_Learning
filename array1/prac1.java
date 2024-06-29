2cdc1e111986c7da04577cebe9b0a55126d444d1
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-06-29 16:29:03 +0530

    prac1.java modified

---
+++ array1/prac1.java
@@ -0,0 +1,27 @@
+package Java_Learning.array1;
+
+//binary search
+
+public class prac1 {
+    // func for binary search
+    public static void binSearch(int arr[], int key) {
+        boolean notFound = true;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                System.out.println("Element found at index " + i);
+                notFound = false;
+                break;
+            }
+        }
+        if (notFound) {
+            System.out.println("No match");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 44, 5, 2, 44, 35, 23 };
+
+        binSearch(arr, 0);
+    }
+}
