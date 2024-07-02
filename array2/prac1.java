313183aba6f0e1324fa78d6d9ef6f0a257594a4f
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-02 20:04:55 +0530

    prac1.java added

---
+++ array2/prac1.java
@@ -0,0 +1,50 @@
+package Java_Learning.array1;
+
+import java.util.Scanner;;
+
+public class prac1 {
+    // func to print max sum of an subarray
+    public static void printSubArray(int array[]) {
+
+        int sum = 0;
+
+        System.out.println("The pairs are :");
+        for (int i = 0; i < array.length; i++) {
+            int start = i;
+
+            for (int j = i; j <= array.length; j++) {
+                int end = j;
+                int tempSum = 0;
+
+                for (int k = start; k < end; k++) {
+                    tempSum += array[k];
+                }
+
+                if (sum < tempSum) {
+                    sum = tempSum;
+                }
+            }
+        }
+
+        System.out.println("Max subarray sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // taking input of array size
+        System.out.print("Enter size of array : ");
+        int size = sc.nextInt();
+
+        // taking input of array
+        int array[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element at index " + i + " : ");
+            array[i] = sc.nextInt();
+        }
+
+        printSubArray(array);
+
+        sc.close();
+    }
+}
