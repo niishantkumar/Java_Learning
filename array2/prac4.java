b78f271e432f1eff2d88374e1b49bc6fcdba69a1
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-03 14:55:11 +0530

    prac4.java modified

---
+++ array2/prac4.java
@@ -0,0 +1,13 @@
+package Java_Learning.array2;
+
+//rainwater trapping problem
+
+public class prac4 {
+    public static void waterTrapped(int arrayOfHeight[]) {
+
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+    }
+}
