314db5e60930606cea5f5803479c253ccafbe8ba
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-04 23:23:34 +0530

    prac1.java modified

--- basicSort/prac1.java
+++ basicSort/prac1.java
@@ -1,5 +1,31 @@
 package Java_Learning.basicSort;
 
+//bubble sort
+
 public class prac1 {
-    
+    // func for bubble sort
+    public static void bubbleSort(int array[]) {
+        int n = array.length;
+        int temp;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int array[] = { 5, 4, 1, 3, 2 };
+
+        bubbleSort(array);
+
+        for(int i = 0; i<array.length; i++){
+            System.err.print(array[i]+" ");
+        }
+    }
 }
