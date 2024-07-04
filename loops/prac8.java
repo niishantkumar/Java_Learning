71d4a1f3314e01470809da68ed86fec7528549a5
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-04 19:40:24 +0530

    prac8.java modified

--- loops/prac8.java
+++ loops/prac8.java
@@ -1,5 +1,46 @@
 package Java_Learning.loops;
 
+import java.util.Scanner;
+
+/*
+You are given two numbers, A and B, where B >= A
+Find the last digit of the answer B!/A! in integer form.
+Note: B!/A! is guaranteed to be an integer as B >= A
+*/
+
 public class prac8 {
-    
+
+    // func to find factorial
+    public static int factorial(int num) {
+        int factorial = 1;
+        for (int i = 2; i <= num; i++) {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    // func to find B!/A!
+    public static int AFac_By_BFac(int a, int b) {
+        int quotient = factorial(b) / factorial(a);
+
+        int lastDigit = quotient % 10;
+
+        return lastDigit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // taking input of A & B
+        System.out.println("Note : B>A");
+        System.out.print("Enter B : ");
+        int b = sc.nextInt();
+        System.out.print("Enter a : ");
+        int a = sc.nextInt();
+
+        System.out.println("Last digit is : " + AFac_By_BFac(a, b));
+
+        sc.close();
+    }
 }
