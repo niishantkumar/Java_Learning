436f4dc65d4f0daf8a3d1fa5c2d09b450c57b0ff
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-25 19:36:44 +0530

    prac1.java modified

---
+++ TwoDArray/prac1.java
@@ -0,0 +1,31 @@
+package Java_Learning.TwoDArray;
+
+import java.util.Scanner;
+
+//creation of 2D Array
+
+public class prac1 {
+    public static void main(String Args[]) {
+        int matrix[][] = new int[3][3];
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        // taking input of matrix elements
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Enter element at index ("+i+","+j+") : ");
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // displaying matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
