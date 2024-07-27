1f274c64c2e7c8b62918f20eb78f5971998a551f
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-27 21:23:06 +0530

    prac3.java modified

--- TwoDArray/prac3.java
+++ TwoDArray/prac3.java
@@ -2,5 +2,56 @@ package Java_Learning.TwoDArray;
 
 //spiral output of 2D array
 public class prac3 {
-    
+    // func to print in spiral form
+    public static void spiralForm(int arr[][]) {
+        int tRows = arr.length - 1;
+        int tCols = arr[0].length - 1;
+
+        int upperRow = 0;
+        int lowerRow = arr.length - 1;
+        int rowRelated_updation = 0;
+        int rightCol = arr.length - 1;
+        int leftCol = 0;
+        int colRelated_updation = 1;
+        int tElements = 0;
+
+        while (tElements < ((tRows + 1) * (tCols + 1))) {
+            // loop for upper row
+            for (int i = rowRelated_updation; i <= tCols - rowRelated_updation; i++) {
+                System.out.print(arr[upperRow][i] + " ");
+                tElements++;
+            }
+
+            // loop for right col
+            for (int i = colRelated_updation; i <= tRows - colRelated_updation; i++) {
+                System.out.print(arr[i][rightCol] + " ");
+                tElements++;
+            }
+
+            // loop for lower row
+            for (int i = tCols - rowRelated_updation; i >= rowRelated_updation; i--) {
+                System.out.print(arr[lowerRow][i] + " ");
+                tElements++;
+            }
+
+            // loop for left row
+            for (int i = tRows - colRelated_updation; i >= colRelated_updation; i--) {
+                System.out.print(arr[i][leftCol] + " ");
+                tElements++;
+            }
+
+            rowRelated_updation++;
+            colRelated_updation++;
+            upperRow++;
+            lowerRow--;
+            rightCol--;
+            leftCol++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        spiralForm(matrix);
+    }
 }
