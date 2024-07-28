f813aa50f9bf4f01c7559969e0436e632ea9cffd
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-28 22:39:48 +0530

    prac7.java modified

--- TwoDArray/prac7.java
+++ TwoDArray/prac7.java
@@ -1,5 +1,30 @@
 package Java_Learning.TwoDArray;
 
+//sum of elements of diagonals of a matrix
+
 public class prac7 {
-    
+    // func for diag sum
+    public static void sum_PS_DiagElements(int matrix[][]) {
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            if (i != (matrix.length / 2)) {
+                sum += matrix[i][i];
+            }
+
+            // if (i != matrix.length - i - 1)
+            sum += matrix[i][matrix.length - i - 1];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 }, { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 24, 25 } };
+
+        sum_PS_DiagElements(matrix);
+    }
+
 }
