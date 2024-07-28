904d2db47d716c4ad89bdacfe2693f2491d3feef
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-07-28 18:30:21 +0530

    prac3.java modified

--- TwoDArray/prac3.java
+++ TwoDArray/prac3.java
@@ -51,7 +51,16 @@ public class prac3 {
     }
 
     public static void main(String[] args) {
-        int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int matrix[][] = { { 1, 2, 3, 4, 22 }, { 5, 6, 7, 8 , 23}, { 9, 10, 11, 12,24 }, { 13, 14, 15, 16,25 }, {17, 18, 19, 20, 21}};
+
+        // displaying matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
         spiralForm(matrix);
     }
 }
