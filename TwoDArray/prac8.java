ed058ef246356a82f7b494ee5117d6be9f3fa8a8
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-10 18:12:16 +0530

    prac8.java modified

--- TwoDArray/prac8.java
+++ TwoDArray/prac8.java
@@ -1,10 +1,46 @@
 package Java_Learning.TwoDArray;
 
+import java.util.Scanner;
+
 //searching in a 2D array
 
 public class prac8 {
-    //main func
+    // func for searching
+    public static void staircaseSearch(int arr[][], int key) {
+        int row = 0, col = arr[0].length - 1;
+
+        while (row < arr.length && col >= 0) {
+            if (arr[row][col] == key) {
+                System.out.println("Found at " + row + "," + col);
+                break;
+            } 
+            else if (arr[row][col] > key) {
+                col--;
+            }
+            else {
+                row++;
+            }
+
+        }
+
+    }
+
+    // main func
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int arr[][] = { { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 27, 29, 37, 48 },
+                { 32, 33, 39, 50 } };
+
+        // taking input of key to be searched
+        int key;
+        System.out.print("Enter key : ");
+        key = sc.nextInt();
+
+        staircaseSearch(arr, key);
+
+        sc.close();
     }
 }
