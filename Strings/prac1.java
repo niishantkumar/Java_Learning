2cc5fab66c87f2d1ff043f6b3db7a29d364d6579
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-11 21:17:47 +0530

    prac1.java modified

--- Strings/prac1.java
+++ Strings/prac1.java
@@ -1,7 +1,30 @@
 package Java_Learning.Strings;
 
+import java.util.Scanner;
+
 //some funcs of strings
 
 public class prac1 {
-    
+    // main func
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // input output of stringSS
+        System.out.print("Enter your full name : ");
+        String name = sc.nextLine();
+        System.out.println("Hi " + name);
+
+        // string length func
+        System.out.println("Your name has " + (name.length() - 1) + " letters");
+
+        // concatenation
+        name = "Mr " + name;
+        System.out.println(name);
+
+        // charAt method
+        System.out.println("Your name starts with letter " + name.charAt(3));
+
+        sc.close();
+
+    }
 }
