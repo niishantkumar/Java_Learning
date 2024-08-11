77c4fd7592cf3d052ebb5c0330e30a10986b89c5
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-11 23:23:43 +0530

    prac2.java modified

--- Strings/prac2.java
+++ Strings/prac2.java
@@ -9,7 +9,7 @@ public class prac2 {
         int end = str.length() - 1;
 
         // loop for comp
-        while (start < str.length()) {
+        while (start < str.length() / 2) {
             if (str.charAt(start) != str.charAt(end))
                 return false;
 
@@ -26,7 +26,7 @@ public class prac2 {
         int end = str.length() - 1;
 
         // loop for comp
-        while (start < str.length()) {
+        while (start < str.length() / 2) {
             if (str.charAt(start) != str.charAt(end)) {
                 System.out.println(str + " is not a palidrome.");
                 break;
@@ -34,7 +34,7 @@ public class prac2 {
             start++;
             end--;
 
-            if (start == str.length()) {
+            if (start >= str.length() / 2) {
                 System.out.println(str + " is a palidrome.");
             }
         }
@@ -49,5 +49,21 @@ public class prac2 {
         palidrome2(word1);
         palidrome2(word2);
         palidrome2(word3);
+
+        if (palidrome(word3)) {
+            System.out.println(word3 + " is a palidrome.");
+        } else {
+            System.out.println(word3 + " is not a palidrome.");
+        }
+        if (palidrome(word1)) {
+            System.out.println(word1 + " is a palidrome.");
+        } else {
+            System.out.println(word1 + " is not a palidrome.");
+        }
+        if (palidrome(word2)) {
+            System.out.println(word2 + " is a palidrome.");
+        } else {
+            System.out.println(word2 + " is not a palidrome.");
+        }
     }
 }
