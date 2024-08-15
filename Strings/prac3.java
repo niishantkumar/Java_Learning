63558a9ca32e7bd20101469948d0a4b843f436d9
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-15 23:03:23 +0530

    prac3.java modified

--- Strings/prac3.java
+++ Strings/prac3.java
@@ -4,5 +4,38 @@ package Java_Learning.Strings;
 //find the shortest path to reach destination
 
 public class prac3 {
-    
+
+    // func to cal shortest dis
+    public static float shortestDis(String str){
+        float dis = 0;
+        int x = 0;
+        int y = 0;
+        char dir;
+
+        for(int i = 0; i<str.length(); i++){
+            dir = str.charAt(i);
+            if (dir == 'N') {
+                y++;
+            }
+            else if (dir == 'S') {
+                y--;
+            }
+            else if(dir == 'E'){
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+
+        dis = (float)Math.sqrt((x*x) + (y*y));
+
+        return dis;
+    }
+
+    public static void main(String[] args) {
+        String str = "WNEENESENNN";
+
+        System.out.println("The shortest distance is : " + shortestDis(str));
+    }
 }
