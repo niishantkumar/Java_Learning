868a3fb9009ca8aa7dd7b8d79c6b923690929df2
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-16 00:06:58 +0530

    prac4.java modified

--- Strings/prac4.java
+++ Strings/prac4.java
@@ -3,5 +3,29 @@ package Java_Learning.Strings;
 //comparison of strings
 
 public class prac4 {
+
+    public static void main(String[] args) {
+        
+        String s1 = "Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+
+        if(s1 == s2){
+            System.out.println("S1 == S2");
+        }
+        if (s1 == s3) {
+            System.out.println("S1 == S3");
+        }
+        else{
+            System.out.println("S1 == S3 is false");
+        }
+
+        if (s1.equals(s3)) {
+            System.out.println("s1.equals(s3)");
+        }
+        if (s1.equals(s2)) {
+            System.out.println("s1.equals(s2)");
+        }
+    }
     
 }
