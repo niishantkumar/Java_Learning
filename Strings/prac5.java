5d5709316ed237ee6be55e06f843a9f9d2945932
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-17 00:46:20 +0530

    prac5.java modified

--- Strings/prac5.java
+++ Strings/prac5.java
@@ -1,12 +1,38 @@
 package Java_Learning.Strings;
 
+import java.util.Scanner;
+
 //substring
 
 public class prac5 {
+    // func for subString
+    public static String subString(String str, int start, int end) {
+
+        String subStr = "";
+        for (int i = start; i < end; i++) {
+            subStr += str.charAt(i);
+        }
+
+        return subStr;
+    }
 
     // main func
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter string : ");
+        String str = sc.nextLine();
+
+        System.out.println("Enter range :");
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        // func made by me
+        System.err.println("Your substring is : " + subString(str, start, end));
+
+        // inbuilt func
+        System.err.println("Your substring is : " + str.substring(0, 9));
+
+        sc.close();
     }
-
 }
