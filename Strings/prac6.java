068eb7a0834808d60049ed2c3dc239de3af39539
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-17 19:49:15 +0530

    prac6.java modified

--- Strings/prac6.java
+++ Strings/prac6.java
@@ -3,8 +3,23 @@ package Java_Learning.Strings;
 //for a ginev set of strings, print the largest
 
 public class prac6 {
-    //main func
+    // func to return largest string
+    public static String largestString(String fruits[]) {
+        String largest = fruits[0];
+
+        for (int i = 1; i < fruits.length; i++) {
+            if (largest.compareTo(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+        return largest;
+    }
+
+    // main func
     public static void main(String[] args) {
-        
+
+        String fruits[] = { "apple", "mango", "banana" };
+
+        System.out.println("Largest string is : " + largestString(fruits));
     }
 }
