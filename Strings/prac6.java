58af561982c45dbd86b3f76ab6475ce101013dd1
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-17 18:38:58 +0530

    prac6.java added

---
+++ Strings/prac6.java
@@ -0,0 +1,10 @@
+package Java_Learning.Strings;
+
+//for a ginev set of strings, print the largest
+
+public class prac6 {
+    //main func
+    public static void main(String[] args) {
+        
+    }
+}
