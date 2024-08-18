7ce45b75437377ca5d22a111b2353df7dcce547a
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-18 20:50:48 +0530

    prac7.java modified

--- Strings/prac7.java
+++ Strings/prac7.java
@@ -3,5 +3,21 @@ package Java_Learning.Strings;
 //use of StringBulider
 
 public class prac7 {
-    
+
+    // main func
+    public static void main(String[] args) {
+
+        StringBuilder alphabet = new StringBuilder("");
+
+        // loop to add new chars to alphabet
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            alphabet.append(ch);
+        }
+
+        System.out.println(alphabet);
+
+        //converting StringBuilder into String
+        alphabet.toString();
+
+    }
 }
