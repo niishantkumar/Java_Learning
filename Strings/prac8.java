569215bb47a3147f74a9cb63f1dc2c61b521b615
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-18 21:25:07 +0530

    prac8.java added

---
+++ Strings/prac8.java
@@ -0,0 +1,11 @@
+package Java_Learning.Strings;
+
+//for a given String convert each the first letter of each word to uppercase
+
+public class prac8 {
+
+    // main func
+    public static void main(String[] args) {
+
+    }
+}
