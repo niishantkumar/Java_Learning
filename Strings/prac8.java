974d65af804e489b090482256eb012d2988874f5
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-21 23:02:09 +0530

    prac8.java modified

--- Strings/prac8.java
+++ Strings/prac8.java
@@ -1,11 +1,39 @@
 package Java_Learning.Strings;
 
+import java.util.Scanner;
+
 //for a given String convert each the first letter of each word to uppercase
 
 public class prac8 {
 
+    // func to convert each of the first letter to upper case
+    public static String toUpperCase(String str) {
+        StringBuilder convrtString = new StringBuilder("");
+
+        convrtString.append(Character.toUpperCase(str.charAt(0)));
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                convrtString.append(str.charAt(i));
+                convrtString.append(Character.toUpperCase(str.charAt(i + 1)));
+                i++;
+            } else {
+                convrtString.append(str.charAt(i));
+            }
+
+        }
+
+        return convrtString.toString();
+    }
+
     // main func
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String str = sc.nextLine();
 
+        System.out.println("Modified string : " + toUpperCase(str));
+
+        sc.close();
     }
 }
