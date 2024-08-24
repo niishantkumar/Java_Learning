40fb07b687ef09cbc5f6e72fe13dddd535bf614b
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-24 15:05:43 +0530

    prac9.java modified

--- Strings/prac9.java
+++ Strings/prac9.java
@@ -6,7 +6,7 @@ String compression
  */
 
 public class prac9 {
-    // func for compression
+    // func for string compression with for loop
     public static String compressString(String str) {
         StringBuilder compStr = new StringBuilder("");
         int count, i, j;
@@ -22,14 +22,37 @@ public class prac9 {
                 }
             }
             compStr.append(count);
-            i = j-1;
+            i = j - 1;
+        }
+        return compStr.toString();
+    }
+
+    // func for string compression with while loop
+    public static String compressString1(String str) {
+        StringBuilder compStr = new StringBuilder("");
+        int count;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            compStr.append(str.charAt(i));
+            count = 1;
+
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+
+            if (count > 1) {
+                compStr.append(count);
+            }
+
         }
         return compStr.toString();
     }
 
     // main func
     public static void main(String[] args) {
-        String str = "aaabbbbbccdddd";
-        System.out.println("Compressed String : " + compressString(str));
+        String str = "aaabccddddeeeefgggg";
+        System.out.println("Compressed String : " + compressString1(str));
     }
 }
