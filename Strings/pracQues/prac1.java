25ff86f6bc670aaaf84c918a459cfc870f89a0a0
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-24 15:32:12 +0530

    prac1.java added

---
+++ Strings/pracQues/prac1.java
@@ -0,0 +1,9 @@
+package Java_Learning.Strings.pracQues;
+
+import java.util.Scanner;
+
+//Count how many times lowercase vowels occurred in a String entered by the user.
+
+public class prac1 {
+    
+}
