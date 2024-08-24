54923f5560108aeffbe3ad82ea46435bc7d3f555
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-24 15:44:25 +0530

    prac2.java added

---
+++ Strings/pracQues/prac2.java
@@ -0,0 +1,12 @@
+package Java_Learning.Strings.pracQues;
+
+/*
+Determine if 2 Strings areanagramsof each other.
+What are anagrams?
+If two strings contain the same characters but in a different order,they can be said to be anagrams.
+Consider race and care
+*/
+
+public class prac2 {
+
+}
