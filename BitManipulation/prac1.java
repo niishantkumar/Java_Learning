244c532516d7df05585753e6121a99a03839df81
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-25 00:58:20 +0530

    prac1.java modified

--- BitManipulation/prac1.java
+++ BitManipulation/prac1.java
@@ -3,5 +3,25 @@ package Java_Learning.BitManipulation;
 //bit manipulation
 
 public class prac1 {
-    
+
+    public static void main(String[] args) {
+        // AND operator
+        System.out.println("5 & 6 : " + (5 & 6));
+
+        // OR operator
+        System.out.println("5 | 6 : " + (5 | 6));
+
+        // XOR operator
+        System.out.println("5 ^ 6 : " + (5 ^ 6));
+
+        // one's complement ~
+        System.out.println("One's complement of 0 : " + (~0));
+
+        // left shift
+        System.out.println("5 << 2 = " + (5 << 2));
+
+        // right shift
+        System.out.println("6 >> 1 = " + (6 >> 1));
+    }
+
 }
