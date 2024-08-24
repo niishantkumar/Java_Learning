f0e5489df359dce4a49d26a0174352cdaa2254ff
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-24 15:33:02 +0530

    prac1.java modified

--- Strings/pracQues/prac1.java
+++ Strings/pracQues/prac1.java
@@ -5,5 +5,33 @@ import java.util.Scanner;
 //Count how many times lowercase vowels occurred in a String entered by the user.
 
 public class prac1 {
-    
+
+    // func to count lowercase vovels in a string
+    public static int countLowerVovl(String str) {
+        int count = 0;
+
+        char ch;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // main func
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter string : ");
+        String str = sc.nextLine();
+
+        System.out.println("Count of vovel is : " + countLowerVovl(str));
+
+        sc.close();
+    }
 }
