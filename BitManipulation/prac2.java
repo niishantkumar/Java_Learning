6f857934ef7df8978c31b1bb74cb289f7100477d
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-25 17:13:37 +0530

    prac2.java modified

--- BitManipulation/prac2.java
+++ BitManipulation/prac2.java
@@ -1,7 +1,28 @@
 package Java_Learning.BitManipulation;
 
+import java.util.Scanner;
+
 //to check if num is even or odd
 
 public class prac2 {
-    
+    // func to check even or odd
+    public static void evenOdd(int num) {
+
+        if ((num & 1) == 0) {
+            System.out.println(num + " is even");
+        } else {
+            System.out.println(num + " is odd");
+        }
+    }
+
+    // main func
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int num = sc.nextInt();
+        evenOdd(num);
+
+        sc.close();
+    }
+
 }
