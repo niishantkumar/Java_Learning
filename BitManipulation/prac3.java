426b1b32f5470c131ad91c64857bd7dd97b2d35a
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-08-25 19:03:20 +0530

    prac3.java modified

--- BitManipulation/prac3.java
+++ BitManipulation/prac3.java
@@ -3,8 +3,17 @@ package Java_Learning.BitManipulation;
 //get ith bit, set ith bit
 
 public class prac3 {
-    //main func
+    // func to find ith bit
+    public static void ithBit(int num, int i) {
+        if ((num & (1 << i)) == 0) {
+            System.out.println("ith bit is 0");
+        } else {
+            System.out.println("ith bit is 1");
+        }
+    }
+
+    // main func
     public static void main(String[] args) {
-        
+        ithBit(10, 2);
     }
 }
