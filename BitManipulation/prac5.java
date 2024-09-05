7ddb804354f4347472258d6f3f342adf2e609aa1
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-05 20:54:11 +0530

    prac5.java modified

--- BitManipulation/prac5.java
+++ BitManipulation/prac5.java
@@ -3,8 +3,16 @@ package Java_Learning.BitManipulation;
 //clear last i bits
 
 public class prac5 {
+    // func to clear last i bits
+    public static int clear_IBits(int num, int i) {
+        int bitMask = (~0) << i; // ~0 = -1
+
+        return num & bitMask;
+    }
+
     // main func
     public static void main(String[] args) {
 
+        System.out.println("15 becomes : " + clear_IBits(15, 2));
     }
 }
