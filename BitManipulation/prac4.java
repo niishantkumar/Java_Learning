e09a655acadc6de1ea5ed88240b1bc4d61b25bed
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-05 20:01:04 +0530

    prac4.java modified

--- BitManipulation/prac4.java
+++ BitManipulation/prac4.java
@@ -3,7 +3,38 @@ package Java_Learning.BitManipulation;
 //update ith bit
 
 public class prac4 {
-    //func to update ith bit
+
+    // func to clear ith bit
+    public static int clear_ithBit(int num, int i) {
+        int bitMask = ~(1 << i);
+
+        return (num & bitMask);
+    }
+
+    // func to set ith bit
+    public static int set_ithBit(int num, int i) {
+        int bitMask = 1 << i;
+
+        return (num | bitMask);
+    }
+
+    // func to update ith bit
+    public static int updateIthBit(int num, int i, int newbit) {
+
+        // 1st approach
+        // if (newbit == 0) {
+        // return (clear_ithBit(num, i));
+
+        // } else {
+        // return (set_ithBit(num, i));
+        // }
+
+        // 2nd approach
+        num = clear_ithBit(num, i);
+        int bitMask = newbit << i;
+        return num | bitMask;
+
+    }
 
     // main func
     public static void main(String[] args) {
