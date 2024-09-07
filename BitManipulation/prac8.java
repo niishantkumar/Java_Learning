0c9e8fbf7a4880039ae05b63973a2cc38898dfb5
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-07 23:33:01 +0530

    prac8.java modified

--- BitManipulation/prac8.java
+++ BitManipulation/prac8.java
@@ -3,9 +3,25 @@ package Java_Learning.BitManipulation;
 //to count set bits in a number
 
 public class prac8 {
+    // func to count bits in a number
+    public static int countBits(int num) {
+        int count = 0;
+
+        while (num > 0) {
+            if ((num & 1) != 0) {
+                count++;
+            }
+
+            num = num >> 1;
+
+        }
+
+        return count;
+    }
 
     // main func
     public static void main(String[] args) {
 
+        System.out.println("Set bits in 10 : "+countBits(10));
     }
 }
