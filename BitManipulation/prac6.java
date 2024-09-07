6662a6426616455ec2b600bfb0762d3d1d5c64bd
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-07 13:30:59 +0530

    prac6.java modified

--- BitManipulation/prac6.java
+++ BitManipulation/prac6.java
@@ -3,10 +3,19 @@ package Java_Learning.BitManipulation;
 //clear range of bits
 
 public class prac6 {
+    // func for clear range of bits
+    public static int clearBitsInRange(int num, int i, int j) {
+        int a = (~0) << (j + 1);
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+
+        return num & bitMask;
+    }
 
     // main func
     public static void main(String[] args) {
 
-    }
+        System.out.println(clearBitsInRange(10, 2, 4));
 
+    }
 }
