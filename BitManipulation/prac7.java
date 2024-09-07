07c943aee13f7ae37770457b195cfab70c656938
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-07 19:54:12 +0530

    prac7.java modified

--- BitManipulation/prac7.java
+++ BitManipulation/prac7.java
@@ -3,9 +3,16 @@ package Java_Learning.BitManipulation;
 //to check if a num is in power of 2
 
 public class prac7 {
+    // func to check whether pow of 2
+    public static boolean powOf2(int num) {
+
+        return (num & (num - 1)) == 0;
+    }
 
     // main func
     public static void main(String[] args) {
+
+        System.out.println(powOf2(9));
 
     }
 }
