5491da13e8b63b5b23bc66c0d1405ca43ee190a5
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-08 23:13:21 +0530

    prac2.java modified

--- BitManipulation/pracQues/prac2.java
+++ BitManipulation/pracQues/prac2.java
@@ -3,5 +3,14 @@ package Java_Learning.BitManipulation.pracQues;
 //Add 1 to an integer using Bit Manipulation
 
 public class prac2 {
-    
+
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x + " + " + 1 + " is " + -(~x));
+        x = -4;
+        System.out.println(x + " + " + 1 + " is " + -(~x));
+        x = 0;
+        System.out.println(x + " + " + 1 + " is " + -(~x));
+    }
+
 }
