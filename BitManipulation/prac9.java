32c6d98ddfc9f13f55e6135b27c2b6fc126b088a
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-08 20:06:27 +0530

    prac9.java modified

--- BitManipulation/prac9.java
+++ BitManipulation/prac9.java
@@ -3,9 +3,23 @@ package Java_Learning.BitManipulation;
 //fast exponentiation
 
 public class prac9 {
+    // func to calculate exponentiation
+    public static int powOf(int num, int pow) {
+        int ans = 1;
+
+        while (pow > 0) {
+            if ((pow & 1) != 0) {
+                ans = ans * num;
+            }
+            num = num * num;
+            pow = pow >> 1;
+        }
+
+        return ans;
+    }
 
     // main func
     public static void main(String[] args) {
-
+        System.err.println("3 power 5 : " + powOf(3, 5));
     }
 }
