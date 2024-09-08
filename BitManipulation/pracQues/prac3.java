830d8178b9692a4a7f26153947b93b60356fc599
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-08 23:35:11 +0530

    prac3.java modified

--- BitManipulation/pracQues/prac3.java
+++ BitManipulation/pracQues/prac3.java
@@ -3,5 +3,13 @@ package Java_Learning.BitManipulation.pracQues;
 //Convert uppercase characters to lowercase using bits
 
 public class prac3 {
-    
+
+    public static void main(String[] args) {
+        // Convert uppercase character to lowercase
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            System.out.print(((char) (ch | ' '))+" ");
+            // prints abcdefghijklmnopqrstuvwxyz
+        }
+    }
+
 }
