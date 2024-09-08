fea6caf846962d4853c72df92826560f2c70f176
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-08 21:06:01 +0530

    prac1.java added

---
+++ BitManipulation/pracQues/prac1.java
@@ -0,0 +1,12 @@
+package Java_Learning.BitManipulation.pracQues;
+
+//Add 1 to an integer using Bit Manipulation
+
+public class prac1 {
+
+    //main func
+    public static void main(String[] args) {
+        
+    }
+    
+}
