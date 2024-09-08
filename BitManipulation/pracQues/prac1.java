128f5b381a0260b0ec636b7cc4259d33abf91d51
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-08 22:51:24 +0530

    prac1.java modified

--- BitManipulation/pracQues/prac1.java
+++ BitManipulation/pracQues/prac1.java
@@ -1,12 +1,19 @@
 package Java_Learning.BitManipulation.pracQues;
 
-//Add 1 to an integer using Bit Manipulation
+//Swap two numbers without using any third variable
 
 public class prac1 {
 
-    //main func
+    // main func
     public static void main(String[] args) {
-        
+        int a = 3, b = 6;
+        System.out.println("Before swapping :\na = " + a + "\nb = " + b);
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swapping :\na = " + a + "\nb = " + b);
+
     }
-    
 }
