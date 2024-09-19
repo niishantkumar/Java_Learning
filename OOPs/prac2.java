9fae048df95795dd3026b33476065f783e141c33
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-19 23:37:46 +0530

    prac2.java modified

--- OOPs/prac2.java
+++ OOPs/prac2.java
@@ -4,4 +4,27 @@ package Java_Learning.OOPs;
 
 public class prac2 {
 
+    public static void main(String[] args) {
+
+        bankAcc user1 = new bankAcc();
+
+        user1.setPass("pass");
+
+        user1.display();
+    }
+
+}
+
+class bankAcc {
+    String name = "\"nish\"";
+    String password;
+
+    void setPass(String pwd) {
+        password = pwd;
+    }
+
+    void display() {
+        System.out.println("Name : " + name);
+        System.out.println("Password : " + password);
+    }
 }
