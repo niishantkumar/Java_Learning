b79b62f8c6ad3f84cf6c1acd6d10e232a105ce14
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-19 22:43:43 +0530

    prac1.java modified

--- OOPs/prac1.java
+++ OOPs/prac1.java
@@ -1,3 +1,33 @@
 package Java_Learning.OOPs;
 
 //simple example of class and object
+
+public class prac1 {
+    public static void main(String[] args) {
+        pen p1 = new pen();
+
+        p1.color = "Red";
+        p1.size = 3;
+
+        p1.display();
+
+    }
+}
+
+class pen {
+    String color;
+    int size;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setSize(int newSize) {
+        size = newSize;
+    }
+
+    void display() {
+        System.err.println("The color is : " + color);
+        System.err.println("The size is : " + size);
+    }
+}
