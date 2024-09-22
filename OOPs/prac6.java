f4576743949b29c033a936407ad065566560cf24
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-22 22:23:23 +0530

    prac6.java modified

--- OOPs/prac6.java
+++ OOPs/prac6.java
@@ -3,5 +3,41 @@ package Java_Learning.OOPs;
 //to show copy constructor
 
 public class prac6 {
-    
+
+    // main func
+    public static void main(String[] args) {
+        BankAcc user1 = new BankAcc();
+        user1.accNo = 1;
+        user1.name = "Nishant";
+        user1.Branch = "Main";
+
+        BankAcc user2 = new BankAcc(user1);
+        user2.accNo = 2;
+        user2.name = "Singh";
+
+        user1.getData();
+        user2.getData();
+
+    }
+}
+
+class BankAcc {
+    int accNo;
+    String name;
+    String Branch;
+
+    BankAcc() {
+
+    }
+
+    BankAcc(BankAcc x) { // copy constructor
+        this.Branch = x.Branch;
+    }
+
+    void getData() {
+        System.out.println("Acc No. : " + accNo);
+        System.out.println("Name : " + name);
+        System.out.println("Branch : " + Branch);
+        System.out.println();
+    }
 }
