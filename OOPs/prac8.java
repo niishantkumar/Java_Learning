4cb8fa44551445f450df66edd38ca6734dfef847
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-25 00:31:44 +0530

    prac8.java modified

--- OOPs/prac8.java
+++ OOPs/prac8.java
@@ -3,5 +3,35 @@ package Java_Learning.OOPs;
 //inheritance
 
 public class prac8 {
-    
+
+    // main func
+    public static void main(String[] args) {
+
+        E_rick rick1 = new E_rick();
+
+        rick1.wheel();
+        rick1.battery();
+
+    }
+
+}
+
+class Auto {
+    String wheel;
+    String engine;
+
+    void wheel() {
+        wheel = "mrf";
+        System.out.println(wheel);
+    }
+
+    void engine() {
+        System.out.println("plagio");
+    }
+}
+
+class E_rick extends Auto {
+    void battery() {
+        System.out.println("Battery");
+    }
 }
