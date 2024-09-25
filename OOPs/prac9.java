ae015b11b211ad85f61b8fd15f5a386aa101ce29
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-25 22:46:32 +0530

    prac9.java modified

--- OOPs/prac9.java
+++ OOPs/prac9.java
@@ -5,7 +5,28 @@ package Java_Learning.OOPs;
 public class prac9 {
 
     public static void main(String[] args) {
-        
+
+        Calculator sum = new Calculator();
+
+        System.out.println("3 + 4 = " + sum.sum(3, 4));
+        System.out.println("3.3 + 4.3 = " + sum.sum(3.3f, 4.3f));
+        System.out.println("3 + 4 + 5 = " + sum.sum(3, 4, 5));
+
     }
 
+}
+
+class Calculator {
+
+    int sum(int a, int b) {
+        return (a + b);
+    }
+
+    float sum(float a, float b) {
+        return (a + b);
+    }
+
+    int sum(int a, int b, int c) {
+        return (a + b + c);
+    }
 }
