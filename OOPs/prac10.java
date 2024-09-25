20ff297859fd0db7175ce1e37d7de8289cc3a066
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-25 23:37:53 +0530

    prac10.java added

--- OOPs/prac10.java
+++ OOPs/prac10.java
@@ -3,5 +3,23 @@ package Java_Learning.OOPs;
 //method overriding
 
 public class prac10 {
-    
+
+    // main func
+    public static void main(String[] args) {
+        Deer d1 = new Deer();
+        d1.eat();
+    }
+
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Eats anything");
+    }
+}
+
+class Deer {
+    void eat() {
+        System.out.println("Eats grass");
+    }
 }
