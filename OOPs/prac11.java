e4a7e5cb86d911aea83dad4d75a263131cd482bc
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-27 22:27:09 +0530

    prac11.java modified

--- OOPs/prac11.java
+++ OOPs/prac11.java
@@ -1,5 +1,39 @@
 package Java_Learning.OOPs;
 
-public class prac11 {
+//abstract class
 
+public class prac11 {
+    public static void main(String[] args) {
+
+        Lion l1 = new Lion();
+        l1.legs();
+
+        Monkey m1 = new Monkey();
+        m1.legs();
+
+        
+    }
+}
+
+abstract class Animal{
+    Animal(){
+        System.out.println("Constructor of abstract class Animal");
+    }
+    void eat() {
+        System.out.println("Animal Eats");
+    }
+
+    abstract void legs();
+}
+
+class Lion extends Animal {
+    void legs() {
+        System.out.println("Lions have four legs");
+    }
+}
+
+class Monkey extends Animal{
+    void legs() {
+        System.out.println("Monkeys have two legs");
+    }
 }
