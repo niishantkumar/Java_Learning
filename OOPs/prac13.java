f961944b29f1ecdf6a03dcae80645d74f639f652
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-28 18:28:52 +0530

    prac13.java modified

--- OOPs/prac13.java
+++ OOPs/prac13.java
@@ -3,5 +3,28 @@ package Java_Learning.OOPs;
 //show multiple inheritance
 
 public class prac13 {
-    
+
+    public static void main(String[] args) {
+        Omnivores m1 = new Omnivores();
+        m1.eatsFlesh();
+        m1.eatsVegetables();
+    }
+}
+
+interface Herbivores {
+    void eatsVegetables();
+}
+
+interface Carnivores {
+    void eatsFlesh();
+}
+
+class Omnivores implements Herbivores, Carnivores {
+    public void eatsFlesh() {
+        System.out.println("Omnivores eat flesh");
+    }
+
+    public void eatsVegetables() {
+        System.out.println("Omnivores eat Vegetables");
+    }
 }
