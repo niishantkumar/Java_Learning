3f8db7eb4221c9a04c4aed88c87e4d997d8103ee
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-28 17:59:27 +0530

    prac12.java modified

--- OOPs/prac12.java
+++ OOPs/prac12.java
@@ -8,28 +8,26 @@ public class prac12 {
         Queen q1 = new Queen();
         q1.moves();
     }
-    
 }
-
 
 interface chessPlayer {
     void moves();
 }
 
 class Queen implements chessPlayer {
-    public void moves(){
+    public void moves() {
         System.out.println("up, down, left, right, diagonally");
     }
 }
 
 class Rook implements chessPlayer {
-    public void moves(){
+    public void moves() {
         System.out.println("up, down, left, right");
     }
 }
 
 class King implements chessPlayer {
-    public void moves(){
+    public void moves() {
         System.out.println("up, down, left, right, diagonally (by one step)");
     }
 }
