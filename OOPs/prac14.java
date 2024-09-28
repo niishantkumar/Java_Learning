ac9ad1008e292d911173a59876043265bf2a5b38
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-29 00:05:40 +0530

    prac14.java modified

--- OOPs/prac14.java
+++ OOPs/prac14.java
@@ -3,5 +3,36 @@ package Java_Learning.OOPs;
 //static keyword
 
 public class prac14 {
-    
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Nishant", 1);
+        Student.school = "DAV Pundag";
+        s1.getData();
+        System.out.println("Percentage : " + Student.percentage(49, 48, 47));
+
+        Student s2 = new Student("Singh", 2);
+        s2.getData();
+        System.out.println("Percentage : " + Student.percentage(47, 48, 47));
+    }
+}
+
+class Student {
+    String name;
+    int roll;
+    static String school;
+
+    Student(String name, int roll) {
+        this.name = name;
+        this.roll = roll;
+    }
+
+    static float percentage(int math, int chem, int phy) {
+        return ((math + chem + phy) / 3);
+    }
+
+    void getData() {
+        System.out.println("Name : " + name);
+        System.out.println("Roll No. : " + roll);
+        System.out.println("School : " + school);
+    }
 }
