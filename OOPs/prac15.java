a5a2b8f83f12d4c61167b1bd943bab29a1393abd
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-29 01:07:10 +0530

    prac15.java modified

--- OOPs/prac15.java
+++ OOPs/prac15.java
@@ -3,5 +3,26 @@ package Java_Learning.OOPs;
 //super keyword
 
 public class prac15 {
-    
+    public static void main(String[] args) {
+
+        E_rick r1 = new E_rick();
+        System.out.println("Colour : " + r1.colour);
+
+    }
+}
+
+class Auto {
+    String colour;
+
+    Auto() {
+        System.out.println("Auto constructor is called");
+    }
+}
+
+class E_rick extends Auto {
+    E_rick() {
+        super(); // this makes call constructor of parent class call first
+        super.colour = "Yellow";
+        System.out.println("E_rick constructor is called");
+    }
 }
