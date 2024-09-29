f3779e41c2c359ebec3a25ce70164fa7a305c0be
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-09-29 23:25:12 +0530

    prac1.java added

---
+++ OOPs/pracQues/prac1.java
@@ -0,0 +1,10 @@
+package Java_Learning.OOPs.pracQues;
+
+/*
+ Print the sum,difference and product of two complex numbers by creating a class named 'Complex' with separate
+ methods for each operation whose real and imaginary parts are entered by the user 
+ */
+public class prac1 {
+
+
+}
