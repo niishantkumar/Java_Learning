ecbc5a1137bc278379d18565634119de95f7c2fc
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-10-01 00:10:02 +0530

    prac1.java modified

--- recursionBasicsP1/prac1.java
+++ recursionBasicsP1/prac1.java
@@ -4,9 +4,19 @@ package Java_Learning.recursionBasicsP1;
 
 public class prac1 {
 
-    //main func
-    public static void main(String[] args) {
-        
+    // recursive func to print from n to 1
+    public static void print_Nto1(int n) {
+
+        System.out.print(n + " ");
+        if (n == 1) {
+            return;
+        }
+        print_Nto1(n - 1);
     }
-    
+
+    // main func
+    public static void main(String[] args) {
+        print_Nto1(5);
+    }
+
 }
