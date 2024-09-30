74206dafa670d0dae7865165d22e2fcd9e7c6266
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-10-01 00:04:48 +0530

    prac1.java added

---
+++ recursionBasicsP1/prac1.java
@@ -0,0 +1,12 @@
+package Java_Learning.recursionBasicsP1;
+
+//print from n to 1(decreasing order)
+
+public class prac1 {
+
+    //main func
+    public static void main(String[] args) {
+        
+    }
+    
+}
