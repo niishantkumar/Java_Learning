80d339e2727757a2692663b80268e6cb6219cbf2
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-10-02 20:44:02 +0530

    prac2.java modified

--- recursionBasicsP1/prac2.java
+++ recursionBasicsP1/prac2.java
@@ -3,5 +3,21 @@ package Java_Learning.recursionBasicsP1;
 //print numbers from n to 1 (increasing order)
 
 public class prac2 {
-    
+
+    // func to print numbers from n to 1
+    public static void printIncreasing(int n) {
+
+        if (n == 1) {
+            System.out.print(n + " ");
+            return;
+        }
+
+        printIncreasing(n - 1);
+        System.out.print(n + " ");
+
+    }
+
+    public static void main(String[] args) {
+        printIncreasing(10);
+    }
 }
