05ca020937d42e10a23680e00f5c27d86781c4e8
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-10-05 20:27:27 +0530

    prac4.java modified

--- recursionBasicsP1/prac4.java
+++ recursionBasicsP1/prac4.java
@@ -4,9 +4,23 @@ package Java_Learning.recursionBasicsP1;
 
 public class prac4 {
 
-    //main func
-    public static void main(String[] args) {
-        
+    // sum of n natural numbers
+    public static int sum_nNatural(int num) {
+        int sum = 0;
+
+        if (num == 1) {
+            return 1;
+        }
+
+        sum = num + sum_nNatural(num - 1);
+
+        return sum;
     }
-    
+
+    // main func
+    public static void main(String[] args) {
+
+        System.out.println("Sum of natural nums till "+10+" is : "+sum_nNatural(10));
+
+    }
 }
