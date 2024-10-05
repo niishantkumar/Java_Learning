ec4d2925ae1efd55d13b15d3dfc1eb2ffba13f42
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-10-05 18:58:48 +0530

    prac4.java added

---
+++ recursionBasicsP1/prac4.java
@@ -0,0 +1,12 @@
+package Java_Learning.recursionBasicsP1;
+
+//sum of n natural numbers
+
+public class prac4 {
+
+    //main func
+    public static void main(String[] args) {
+        
+    }
+    
+}
