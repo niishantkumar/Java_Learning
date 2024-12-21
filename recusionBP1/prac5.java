425fdd52a0d13b4ff0a6d7c67a7ef2f6b5e6ec27
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 17:31:44 +0530

    prac5.java modified

--- recusionBP1/prac5.java
+++ recusionBP1/prac5.java
@@ -1,8 +1,19 @@
 package Java_Learning.recusionBP1;
 
 public class prac5 {
+    //nth fibonacci term
+    public static int fibo(int n){
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        return fibo(n-1) + fibo(n-2);
+    }
     
     public static void main(String[] args) {
-        
+        System.out.println(fibo(6));
     }
 }
