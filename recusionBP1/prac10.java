1f00e12dab145cfd070b7c261d7dc0f0b6b21694
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 22:34:45 +0530

    prac10.java modified

--- recusionBP1/prac10.java
+++ recusionBP1/prac10.java
@@ -1,5 +1,17 @@
 package Java_Learning.recusionBP1;
 
 public class prac10 {
+    //x^n
+    public static int pow(int num, int pow){
+        if(pow == 0){
+            return 1;
+        }
+
+        return num* pow(num,pow-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("5^3 = "+pow(5, 4));
+    }
     
 }
