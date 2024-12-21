b2dc4aac9849d76cee2ed3163b2b6561dd02aa5b
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 15:15:50 +0530

    prac1.java modified

--- recusionBP1/prac1.java
+++ recusionBP1/prac1.java
@@ -3,18 +3,19 @@ package Java_Learning.recusionBP1;
 //print number in decresing order
 
 public class prac1 {
-    //func to print number in decresing order
-    public static void decresing(int num) {
-        if (num == 1) {
-            System.out.print(num);
-            return;
+        //func to print number in decresing order
+        public static void decresing(int num) {
+            if (num == 1) {
+                System.out.print(num);
+                return;
+            }
+    
+            System.out.print(num+" ");
+            decresing(num-1);
+            
         }
 
-        System.err.println();
-        
-    }
-
     public static void main(String[] args) {
-        
+        decresing(5);
     }
 }
