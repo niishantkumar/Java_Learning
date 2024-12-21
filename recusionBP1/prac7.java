9617b223d46a1eb16cb792aa6e92a1c10be1fe88
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 19:29:54 +0530

    prac7.java modified

--- recusionBP1/prac7.java
+++ recusionBP1/prac7.java
@@ -1,5 +1,22 @@
 package Java_Learning.recusionBP1;
 
 public class prac7 {
-    
+    //first occrance
+    public static int index(int arr[],int i, int key){
+        if(i == arr.length){
+            return -1;
+        }
+
+        if (arr[i] == key) {
+            return i;
+        }
+
+        return index(arr, i+1, key);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,7};
+
+        System.out.println(index(arr, 0, 6));
+    }
 }
