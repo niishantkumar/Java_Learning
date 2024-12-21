407ad5778049107717f868f06cfffda1db9289e8
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 16:51:46 +0530

    prac2.java modified

--- recusionBP1/prac2.java
+++ recusionBP1/prac2.java
@@ -1,5 +1,19 @@
 package Java_Learning.recusionBP1;
 
 public class prac2 {
+    //func to print number in incresing order
+    public static void incresingOrd(int num){
+        if(num == 1){
+            System.out.print(num+" ");
+            return;
+        }
+
+        incresingOrd(num-1);
+        System.out.print(num+" ");
+    }
+
+    public static void main(String[] args) {
+        incresingOrd(6);
+    }
     
 }
