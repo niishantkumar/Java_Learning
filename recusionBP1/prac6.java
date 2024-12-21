da13d994c784980429f30ba704337e2bed1591ba
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 19:01:35 +0530

    prac6.java modified

--- recusionBP1/prac6.java
+++ recusionBP1/prac6.java
@@ -1,8 +1,25 @@
 package Java_Learning.recusionBP1;
 
 public class prac6 {
+    //array is sorted or not
+    public static boolean isSorted(int arr[], int n){
+        if(n == arr.length -1){
+            return true;
+        }
+
+        int e1 = arr[n];
+        int e2 = arr[n+1];
+
+        if (e1 > e2) {
+            return false;
+        }
+
+        return isSorted(arr, n+1);
+    }
 
     public static void main(String[] args) {
-        
+        int arr[] = {1,2,3,4,5,6,7,8};
+
+        System.out.println(isSorted(arr, 0));
     }
 }
