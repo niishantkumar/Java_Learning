49f80d5246823186cb74fc586562112fbdc9d5c5
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 17:03:46 +0530

    prac3.java modified

--- recusionBP1/prac3.java
+++ recusionBP1/prac3.java
@@ -1,8 +1,17 @@
 package Java_Learning.recusionBP1;
 
 public class prac3 {
+     //func for factorial
+     public static int fact(int num){
+        if(num == 1 || num == 0){
+            return 1;
+        }
+
+        return num * fact(num - 1);
+    }
+    
    
     public static void main(String[] args) {
-        
+        System.out.println(fact(5));
     }
 }
