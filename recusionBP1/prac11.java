24ccdc15f7f7e84e7fbf7ede4b40186630722a1a
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 23:12:47 +0530

    prac11.java added

--- recusionBP1/prac11.java
+++ recusionBP1/prac11.java
@@ -1,8 +1,26 @@
 package Java_Learning.recusionBP1;
 
 public class prac11 {
-    
+    public static int pow(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        int halfPowSq = pow(x, n / 2) * pow(x, n / 2);
+
+        /*
+         * int halfPow = pow(x, n/2);
+         * int halfPowSquare = halfPow * halfPow;
+         */
+
+        if (n % 2 != 0) {
+            halfPowSq = halfPowSq * x;
+        }
+
+        return halfPowSq;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println("5^3 = " + pow(5, 3));
     }
 }
