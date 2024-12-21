4f7ebb23e832de6568c4fc37e6486f9d92a586ba
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-21 17:14:21 +0530

    prac4.java modified

--- recusionBP1/prac4.java
+++ recusionBP1/prac4.java
@@ -1,9 +1,19 @@
 package Java_Learning.recusionBP1;
 
 public class prac4 {
+    //sum of n natural numbers
+    public static int sumNaturalNum(int n){
+        if(n == 1){
+            return 1;
+        }
+
+        int sum = n + sumNaturalNum(n-1);
+
+        return sum;
+    }
 
     public static void main(String[] args) {
-        
+        System.out.println("Sum of 8 natural numbers : "+sumNaturalNum(8));
     }
     
 }
