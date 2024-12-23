fc1a99bc568c90af158b63dc061491bec8e7e83e
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-23 20:40:51 +0530

    prac2.java modified

--- recusionBP2/prac2.java
+++ recusionBP2/prac2.java
@@ -1,5 +1,27 @@
 package Java_Learning.recusionBP2;
 
+//tiling problem
+
 public class prac2 {
-    
+    // recursive func for counting number of ways tiling can be done
+    public static int countWays(int breadth) {
+        int count = 0;
+        if (breadth == 0) {
+            return 1;
+        }
+
+        if (breadth % 2 == 0) {
+            breadth = breadth - 2;
+            count = 2;
+        } else {
+            breadth = breadth - 1;
+            count = 1;
+        }
+
+        return count + countWays(breadth);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countWays(1));
+    }
 }
