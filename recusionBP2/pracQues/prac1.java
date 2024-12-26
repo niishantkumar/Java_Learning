c0cf7a40baf63c40e35661f35a994c26a7a6804f
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-26 23:21:40 +0530

    prac1.java modified

--- recusionBP2/pracQues/prac1.java
+++ recusionBP2/pracQues/prac1.java
@@ -6,5 +6,22 @@ package Java_Learning.recusionBP2.pracQues;
  */
 
 public class prac1 {
-    
+    public static void allOccurance(int arr[], int key, int n) { // n -> position from where to be searched
+
+        if (n == arr.length) {
+            return;
+        }
+
+        if (arr[n] == key) {
+            System.out.print(n + " ");
+        }
+
+        allOccurance(arr, key, n + 1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
+
+        allOccurance(arr, 2, 0);
+    }
 }
