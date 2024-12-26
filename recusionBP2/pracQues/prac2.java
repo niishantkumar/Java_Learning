ab22b8ffe5d9be36e991f27e1152cb61e265ad5b
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-26 23:36:20 +0530

    prac2.java added

--- recusionBP2/pracQues/prac2.java
+++ recusionBP2/pracQues/prac2.java
@@ -1,5 +1,31 @@
 package Java_Learning.recusionBP2.pracQues;
 
+/*
+ * You are given a number (eg - 2019), convert it into a String of english
+ * like “two zero one nine”.  Use a recursive function to solve this problem.
+ */
+
 public class prac2 {
-    
+
+    public static void digiToWord(int num, String arr[]) {
+
+        if (num == 0) {
+            return;
+        }
+
+        int currentDigi = num % 10;
+
+        digiToWord(num / 10, arr);
+
+        System.out.print(arr[currentDigi] + " ");
+    }
+
+    public static void main(String[] args) {
+
+        String arr[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        int num = 1947;
+
+        digiToWord(num, arr);
+
+    }
 }
