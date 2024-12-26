b2c9cebcf27912c8e8f46df0ccffeac386e7ee67
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-26 23:13:38 +0530

    prac1.java added

---
+++ recusionBP2/pracQues/prac1.java
@@ -0,0 +1,10 @@
+package Java_Learning.recusionBP2.pracQues;
+
+/*
+ * For a given integer array of size N.You have to find all the occurrences(indices)
+ * of a given element(Key) and print them. Use a recursive function to solve this problem.
+ */
+
+public class prac1 {
+    
+}
