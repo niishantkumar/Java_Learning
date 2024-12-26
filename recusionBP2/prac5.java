ecd188a0c346cf52f7908fbd948c1bd8064cfbaa
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-26 21:40:00 +0530

    prac5.java modified

--- recusionBP2/prac5.java
+++ recusionBP2/prac5.java
@@ -3,5 +3,20 @@ package Java_Learning.recusionBP2;
 //to print all combinations of binary stinr of size n,  but it should not contain consecutive 1's
 
 public class prac5 {
-    
+    public static void printBinStr(int n, int lastBit, String str) {
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
+
+        printBinStr(n - 1, 0, str + '0');
+
+        if (lastBit == 0) {
+            printBinStr(n - 1, 1, str + '1');
+        }
+    }
+
+    public static void main(String[] args) {
+        printBinStr(4, 0, "");
+    }
 }
