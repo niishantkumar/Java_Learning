631001f7d5b42a1c08c53a54eb8d5c5992fcc10e
Nishant Kumar <nishant.kumar2839@gmail.com>
2024-12-27 17:08:32 +0530

    prac3.java modified

--- recusionBP2/pracQues/prac3.java
+++ recusionBP2/pracQues/prac3.java
@@ -1,5 +1,22 @@
 package Java_Learning.recusionBP2.pracQues;
 
+//Write a program to findLength of a String using Recursion
+
 public class prac3 {
-    
+
+    public static int lenStr(String str, String newStr, int i) {
+
+        if (str.equals(newStr)) {
+            return i;
+        }
+
+        newStr += str.charAt(i);
+
+        return lenStr(str, newStr, ++i);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Length of Recursion : " + lenStr("Recursion", "", 0));
+    }
+
 }
