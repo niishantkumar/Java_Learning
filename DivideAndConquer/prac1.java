bd6883383aee2fa3db3d485a2698df5b4142d812
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-01 23:56:50 +0530

    prac1.java modified

--- DivideAndConquer/prac1.java
+++ DivideAndConquer/prac1.java
@@ -15,7 +15,7 @@ public class prac1 {
         int mid = si + (ei - si) / 2;
 
         mergeSort(arr, si, mid); // for left part
-        mergeSort(arr, mid + 1, ei); // fir right part
+        mergeSort(arr, mid + 1, ei); // for right part
         merge(arr, si, mid, ei);
     }
 
@@ -23,9 +23,9 @@ public class prac1 {
     public static void merge(int arr[], int si, int mid, int ei) {
         int temp[] = new int[ei - si + 1];
 
-        int i = si;
-        int j = mid + 1;
-        int k = 0;
+        int i = si; //iterator for left part
+        int j = mid + 1; //iterator for right part
+        int k = 0; //iterator for temp
 
         while (i <= mid && j <= ei) {
             if (arr[i] < arr[j]) {
@@ -39,14 +39,17 @@ public class prac1 {
             k++;
         }
 
+        //for leftover left part
         while (i <= mid) {
             temp[k++] = arr[i++];
         }
 
+        //for leftover right part
         while (j <= ei) {
             temp[k++] = arr[j++];
         }
 
+        //for copying from temp to arr
         for (k = 0, i = si; k < temp.length; k++, i++) {
             arr[i] = temp[k];
         }
