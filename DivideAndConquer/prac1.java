ec627de6ff3ef82c1d7f1ac7d3cbfab95cc3aa56
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-01 21:14:59 +0530

    prac1.java modified

--- DivideAndConquer/prac1.java
+++ DivideAndConquer/prac1.java
@@ -3,5 +3,64 @@ package Java_Learning.DivideAndConquer;
 //merge sort
 
 public class prac1 {
-    
+
+    // func for merge sort
+    public static void mergeSort(int arr[], int si, int ei) {
+
+        // base case
+        if (si >= ei) {
+            return;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        mergeSort(arr, si, mid); // for left part
+        mergeSort(arr, mid + 1, ei); // fir right part
+        merge(arr, si, mid, ei);
+    }
+
+    // func to merge sorted array
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+
+        int i = si;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 23, 1, 4, 6, 6, 2, 7, 8, 3, 2, 6 };
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
 }
