3303a4738f9ac7b4b7beac83e45674c5dbf03867
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-02 21:32:38 +0530

    prac2.java added

---
+++ DivideAndConquer/pracQues/prac2.java
@@ -0,0 +1,11 @@
+package Java_Learning.DivideAndConquer.pracQues;
+
+//an array is sorted at a pivot point. find the index of given element
+
+public class prac2 {
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
