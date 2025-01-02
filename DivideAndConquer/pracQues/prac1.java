b17751527bf55d1b32f2432b70110ff715a84329
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-02 19:32:00 +0530

    prac1.java added

---
+++ DivideAndConquer/pracQues/prac1.java
@@ -0,0 +1,11 @@
+package Java_Learning.DivideAndConquer.pracQues;
+
+//an array is rotated at a pivot...find the index of a given element
+
+public class prac1 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
