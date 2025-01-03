b51465db90d46ed00fcae9d2363a6d6cdc7f099c
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-03 21:11:13 +0530

    prac4.java added

---
+++ DivideAndConquer/pracQues/prac4.java
@@ -0,0 +1,16 @@
+package Java_Learning.DivideAndConquer.pracQues;
+
+//Given an array nums of size n, returnthe majority element.(MEDIUM)
+//The majority element is the element that appears more than ⌊n/2⌋ times.
+//You may assume that the majority element always exists in the array.
+//Sample Input 1: nums = [3,2,3]
+//Sample Output 1: 3
+//Sample Input 2: nums = [2,2,1,1,1,2,2]
+//Sample Output 2: 2
+//Constraints(extra Conditions):
+//●n == nums.length
+//●1 <= n <= 5 * 104
+//●-109 <= nums[i] <= 109
+public class prac4 {
+    
+}
