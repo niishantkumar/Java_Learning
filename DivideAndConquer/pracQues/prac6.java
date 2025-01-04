ad260131f8152a9f7fc8f87fa9a4063465101e21
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-04 19:29:10 +0530

    prac6.java modified

--- DivideAndConquer/pracQues/prac6.java
+++ DivideAndConquer/pracQues/prac6.java
@@ -1,6 +1,25 @@
 package Java_Learning.DivideAndConquer.pracQues;
 
 //Given an array of integers. Find the Inversion Count in the array.
+
 public class prac6 {
-    
+    public static int inversion(int arr[]) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 1, 3, 5};
+
+        System.out.print("Invision is : " + inversion(arr));
+    }
 }
