3d76869583813ae0f73398f3c403d9303d1cc262
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-13 22:40:09 +0530

    prac2.java modified

--- Backtracking/prac2.java
+++ Backtracking/prac2.java
@@ -3,5 +3,28 @@ package Java_Learning.Backtracking;
 //subset of a string
 
 public class prac2 {
-    
+
+    // func for subset
+    public static void subset(String str, String ans, int i) {
+
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("Null");
+            } else {
+                System.err.println(ans);
+            }
+
+            return;
+        }
+
+        subset(str, ans + str.charAt(i), i + 1);
+        subset(str, ans, i + 1);
+
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+
+        subset(str, "", 0);
+    }
 }
