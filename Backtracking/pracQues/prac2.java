54c0f73225fef410748d1fb547b3ebe99ca3fde9
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-19 18:46:37 +0530

    prac2.java added

---
+++ Backtracking/pracQues/prac2.java
@@ -0,0 +1,12 @@
+package Java_Learning.Backtracking.pracQues;
+
+//keypad combination
+
+public class prac2 {
+    
+
+    //main func
+    public static void main(String[] args) {
+        
+    }
+}
