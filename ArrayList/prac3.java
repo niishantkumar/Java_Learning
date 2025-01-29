c0921ce46c8b20302f18160f3767f55cc0b0da02
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-30 00:08:37 +0530

    prac3.java modified

--- ArrayList/prac3.java
+++ ArrayList/prac3.java
@@ -1,7 +1,34 @@
 package Java_Learning.ArrayList;
 
+import java.util.ArrayList;
+
 //swap two values/elements in ArrayList
 
 public class prac3 {
-    
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(6);
+        list.add(0);
+        list.add(4);
+        list.add(10);
+        list.add(8);
+
+        int ind1 = 0;
+        int ind2 = 3;
+
+        System.out.println("Before swapping :");
+        System.out.println(list + "\n");
+
+        // swap
+        int temp = list.get(ind2);
+        list.set(ind2, list.get(ind1));
+        list.set(ind1, temp);
+
+        System.out.println("After swapping :");
+        System.out.println(list + "\n");
+
+    }
+
 }
