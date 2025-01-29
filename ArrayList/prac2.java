1b3a66dbe502684763c01f1e5effb9cc37430c05
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-29 23:53:05 +0530

    prac2.java modified

--- ArrayList/prac2.java
+++ ArrayList/prac2.java
@@ -2,6 +2,26 @@ package Java_Learning.ArrayList;
 
 //find max in an ArrayList
 
+import java.util.ArrayList;
+
 public class prac2 {
-    
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(6);
+        list.add(0);
+        list.add(4);
+        list.add(10);
+        list.add(8);
+
+        int max = list.get(0);
+
+        for (int i = 1; i < list.size(); i++) {
+            max = Math.max(max, list.get(i));
+        }
+
+        System.out.println(max);
+    }
+
 }
