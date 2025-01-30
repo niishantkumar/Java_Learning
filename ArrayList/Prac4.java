4ef9f684dfdc8445a0e0d9f6f08a102d915e1802
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-01-30 17:30:57 +0530

    Prac4.java modified

--- ArrayList/Prac4.java
+++ ArrayList/Prac4.java
@@ -2,6 +2,29 @@ package Java_Learning.ArrayList;
 
 //Sorting an ArrayList
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Prac4 {
-    
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(6);
+        list.add(0);
+        list.add(4);
+        list.add(10);
+        list.add(8);
+
+        // sort in ascending order
+        Collections.sort(list);
+        System.out.println("Sorted list in asc ord :");
+        System.out.println(list);
+
+        // sort in descending order
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println("Sorted list in desc ord :");
+        System.out.println(list);
+    }
+
 }
