74109d11956a44ecf7c7849dd0bd43f461f17bf9
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-02-01 23:11:48 +0530

    Prac8.java modified

--- ArrayList/Prac8.java
+++ ArrayList/Prac8.java
@@ -1,12 +1,38 @@
 package Java_Learning.ArrayList;
 
-//pair sum - 1
+import java.util.ArrayList;
+
+//pair sum - 1 (Brute Force)
 
 public class Prac8 {
 
-    //main func
-    public static void main(String[] args) {
-        
+    // func to check pair sum
+    static void pairSum(ArrayList<Integer> list, int tgt) {
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            int fstTerm = list.get(i);
+            for (int j = i + 1; j < list.size(); j++) {
+                if (fstTerm + list.get(j) == tgt) {
+
+                    System.out.println(fstTerm + " + " + list.get(j) + " = " + tgt);
+                    return;
+                }
+            }
+        }
     }
-    
+
+    // main func
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        pairSum(list, 5);
+
+    }
+
 }
