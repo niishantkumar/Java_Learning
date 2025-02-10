b6b20e646a579035991676f60fcd51498f256ff6
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-02-10 19:43:02 +0530

    Prac2.java modifieded

--- ArrayList/pracQues/Prac2.java
+++ ArrayList/pracQues/Prac2.java
@@ -1,5 +1,7 @@
 package Java_Learning.ArrayList.pracQues;
 
+import java.util.ArrayList;
+
 /*
   Lonely Numbers in ArrayList(MEDIUM).
   You are given an integer arraylist nums. A number x is lonely when it appears only once, and no adjacent numbers (i.e. x + 1 and x - 1)
@@ -20,6 +22,59 @@ package Java_Learning.ArrayList.pracQues;
   Note that [5, 1] may also be returned.
  */
 
+//*********brute force*************
+
 public class Prac2 {
+
+    // main func
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        list.add(3);
+
+        Solution ob = new Solution();
+        ob.lonelyNums(list);
+
+    }
+
+}
+
+class Solution {
+    // func to find lonely number
+    void lonelyNums(ArrayList<Integer> list) {
+        int n = list.size();
+
+        if (n == 1) {
+            System.out.println(list.get(0) + " is a lonely number");
+            return;
+        }
+
+        ArrayList<Integer> temp = new ArrayList<>(); //temporary arrList to store lonely numbs
+        boolean isLonely;
+
+        for (int i = 0; i < n; i++) {
+            isLonely = true;  // Reset isLonely flag for each number
+
+            for (int j = 0; j < n; j++) {
+                if (j == i) {
+                    continue;
+                }
+
+                if (list.get(j) == list.get(i) || list.get(j) == list.get(i) - 1 || list.get(j) == list.get(i) + 1) {
+                    isLonely = false;
+                    break; //if not lonely then no need to check further
+                }
+            }
+
+            if (isLonely) {
+                temp.add(list.get(i));
+            }
+        }
+
+        System.out.println(temp);
+    }
 
 }
