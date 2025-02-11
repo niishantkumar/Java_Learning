6194f004b1d8bf3c4810c3dcb269fe9e1bdac746
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-02-11 22:22:59 +0530

    Prac5.java modified

--- ArrayList/pracQues/Prac5.java
+++ ArrayList/pracQues/Prac5.java
@@ -5,6 +5,46 @@ package Java_Learning.ArrayList.pracQues;
 //tc = O(n)
 //sc = O(1)
 
+import java.util.ArrayList;
+
 public class Prac5 {
 
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+
+        Solution ob = new Solution();
+        System.out.println(ob.mostFrequent(list, 2));
+    }
+}
+
+class Solution {
+    public int mostFrequent(ArrayList<Integer> nums, int key) {
+        int[] result = new int[1000]; // Frequency array
+
+        // Count occurrences of numbers appearing after 'key'
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums.get(i) == key) {
+                result[nums.get(i + 1) - 1]++;
+            }
+        }
+
+        // Find the most frequent number
+        int max = Integer.MIN_VALUE;
+        int ans = 0;
+
+        for (int i = 0; i < 1000; i++) {
+            if (result[i] > max) {
+                max = result[i];
+                ans = i + 1;
+            }
+        }
+
+        return ans;
+    }
 }
