da90e440029448b9f89e717ec3a00c117893d2ec
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-02-12 12:01:04 +0530

    Prac6.java added

---
+++ ArrayList/pracQues/Prac6.java
@@ -0,0 +1,15 @@
+package Java_Learning.ArrayList.pracQues;
+
+/*
+  Beautiful ArrayList(MEDIUM)
+  An Arraylist nums of size n is beautiful if:
+  nums is a permutation of the integers in the range [1, n].For every 0 <= i < j < n, there is no index k with i < k < j where 
+  2*nums.get(k) == nums.get(i) + nums.get(j). Given the integer n, return any beautiful arraylist nums of size n. There will be atleast one
+  valid answer for the given n.
+  
+  Sample Input 1: n = 4
+  Sample Output 1:  [2,1,4,3]
+ */
+public class Prac6 {
+
+}
