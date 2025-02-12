73c1a20bc056cd90b130ae87782d4e7903df215b
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-02-12 12:33:04 +0530

    Prac6.java modified

--- ArrayList/pracQues/Prac6.java
+++ ArrayList/pracQues/Prac6.java
@@ -10,6 +10,57 @@ package Java_Learning.ArrayList.pracQues;
   Sample Input 1: n = 4
   Sample Output 1:  [2,1,4,3]
  */
+
+
+//Time Complexity	O(n log n)
+//Space Complexity	O(n)
+
+import java.util.ArrayList;
+
 public class Prac6 {
+
+    public static void main(String[] args) {
+        Solution ob = new Solution(5);
+
+        ob.beautifulArray();
+
+    }
+
+}
+
+class Solution {
+    int n;
+
+    Solution(int n) {
+        this.n = n;
+    }
+
+    // func to find beautiful array
+    void beautifulArray() {
+        ArrayList<Integer> beautiful = new ArrayList<>();
+        beautiful.add(1);
+
+        while (beautiful.size() < n) {
+            ArrayList<Integer> temp = new ArrayList<>();
+
+            for (int i = 0; i < beautiful.size(); i++) {
+                if (beautiful.get(i) * 2 - 1 <= n) {
+                    temp.add(beautiful.get(i) * 2 - 1);
+                }
+            }
+
+            for (int i = 0; i < beautiful.size(); i++) {
+                if (beautiful.get(i) * 2 <= n) {
+                    temp.add(beautiful.get(i) * 2);
+                }
+            }
+
+            beautiful = temp;
+
+        }
+
+        System.out.println(beautiful);
+
+    }
 
 }
