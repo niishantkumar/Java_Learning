fb1b709d6fbbbe197c12da918a0479193ab2e885
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-02-22 17:45:17 +0530

    Prac2.java modified

--- linkedListP1/Prac2.java
+++ linkedListP1/Prac2.java
@@ -1,7 +1,99 @@
 package Java_Learning.linkedListP1;
 
-//find and remove nth node from end
+//to delete nth node from end
 
 public class Prac2 {
+    public static void main(String[] args) {
 
+        LinkedList sll = new LinkedList();
+
+        sll.addNode(1);
+        sll.addNode(2);
+        sll.addNode(3);
+        sll.addNode(4);
+
+        sll.print();
+
+        System.out.println(sll.size());
+
+        sll.deleteNthFromEnd(2);
+
+        sll.print();
+
+    }
+
+}
+
+class LinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node head;
+    static Node tail;
+
+    // func to calc size of linkedList
+    int size() {
+        Node temp = head;
+        int size = 0;
+
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+
+        return size;
+    }
+
+    void addNode(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // if nodes are there in linked list
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // func to delete nth node from end
+    void deleteNthFromEnd(int n) {
+        int size = size();
+
+        if (n == size) {
+            head = head.next;
+            return;
+        }
+
+        int i = 1;
+        int indTofind = size - n;
+        Node prev = head;
+        while (i < indTofind) {
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+    }
+
+    // func to print linkedList
+    void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+
+    }
 }
