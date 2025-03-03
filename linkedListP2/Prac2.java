c6d1478e8edcbafbac1ac37dedd7ae1de63a281a
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-03-03 22:27:20 +0530

    Prac2.java added

---
+++ linkedListP2/Prac2.java
@@ -0,0 +1,63 @@
+package Java_Learning.linkedListP2;
+
+//remove cycle in linkedList
+
+public class Prac2 {
+
+}
+
+class LinkedList {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    // func to add node in LinkedList
+    void addNode(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // func to form cyclic linkedList
+    void cycle() {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = head;
+    }
+
+    // func to detect a cycle in linkedList
+    boolean isCycle() {
+
+        Node slow = head;
+        Node fast = head;
+
+        // detect cycle
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next; // one step forward
+            fast = fast.next.next; // two step forward
+
+            if (fast == slow) {
+                return true;// cycle exists
+            }
+        }
+
+        return false;// cycle does not exist
+    }
+}
