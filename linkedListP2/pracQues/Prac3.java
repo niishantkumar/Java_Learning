8f375e5b55afb22575f83ceaf1efbf38f53f2396
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-03-20 14:32:47 +0530

    Prac3.java added

---
+++ linkedListP2/pracQues/Prac3.java
@@ -0,0 +1,12 @@
+package Java_Learning.linkedListP2.pracQues;
+
+/* 
+    Swapping Nodes in a Linked List
+
+    Sample Input 1 : 1->2->3->4,  x = 2, y = 4
+    Sample Output 1 : 1->4->3->2
+ */
+
+public class Prac3 {
+    
+}
