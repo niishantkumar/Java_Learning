1bc66fadedf5cdfcd15f8f1c5e47d9f1fc6ec098
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-03-21 23:46:33 +0530

    Prac4.java added

---
+++ linkedListP2/pracQues/Prac4.java
@@ -0,0 +1,16 @@
+package Java_Learning.linkedListP2.pracQues;
+
+/*
+    --------------Odd Even Linked List---------------------
+
+    We have a Linked List of integers, write a function to modify the linked list such that all even
+    numbers appear before all the odd numbers in the modified linked list. Also, keep the order of
+    even and odd numbers same.
+
+    Sample Input 1 : 8->12->10->5->4->1->6->NULL
+    Sample Output 1 : 8->12->10->4->6->5->1->NULL
+ */
+
+public class Prac4 {
+    
+}
