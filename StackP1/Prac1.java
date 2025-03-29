96bc057fe74d88b2fa8e82ad6449d46d1d0e1ded
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-03-29 22:45:12 +0530

    Prac1.java added and modified

---
+++ StackP1/Prac1.java
@@ -0,0 +1,58 @@
+package Java_Learning.StackP1;
+
+import java.util.ArrayList;
+
+public class Prac1 {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        
+        //func to if stack is empty
+        public static boolean isEmpty(){
+            return list.size() == 0;
+        }
+
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+
+        //pop
+        public static int pop(){
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = list.get(list.size() - 1);
+
+            list.remove(list.size() - 1);
+
+            return top;
+        }
+
+        //peek
+        public static int peek(){
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s1 = new Stack();
+
+        s1.push(1);
+        s1.push(2);
+        s1.push(3);
+
+        System.out.println(Stack.list);
+
+        System.out.println(s1.peek()); 
+
+        System.out.println(s1.pop());
+
+        System.out.println(Stack.list);
+    }
+    
+}
