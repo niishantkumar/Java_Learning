f7d696bdae04b0577369aadf97f34e6ee56e3de4
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-03-31 17:10:40 +0530

    Prac2.java modified

--- StackP1/Prac2.java
+++ StackP1/Prac2.java
@@ -1,5 +1,94 @@
 package Java_Learning.StackP1;
 
+//creation of Stack using LinkedList
+
 public class Prac2 {
-    
+    // Node class
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // class for Stack
+    static class Stack {
+        static Node head = null;
+
+        // func to check if empty
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        // push
+        public static void push(int data) {
+            Node newNode = new Node(data);
+
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        // pop
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+
+            return head.data;
+        }
+
+        // main func
+        public static void main(String[] args) {
+            Stack s1 = new Stack();
+
+            s1.push(1);
+            s1.push(2);
+            s1.push(3);
+            s1.push(4);
+
+            while (!s1.isEmpty()) {
+                System.out.println(s1.peek());
+                s1.pop();
+            }
+
+            System.out.println(s1.pop());
+        }
+    }
+
+    // main func
+    public static void main(String[] args) {
+        Stack s1 = new Stack();
+
+        s1.push(1);
+        s1.push(2);
+        s1.push(3);
+        s1.push(4);
+
+        while (!s1.isEmpty()) {
+            System.out.println(s1.peek());
+            s1.pop();
+        }
+
+        System.out.println(s1.pop());
+    }
+
 }
