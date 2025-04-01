eeeb6ebf6fc02d1d58f2a2c243ad773a5817c12f
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-04-01 17:35:27 +0530

    Prac4.java modified

--- StackP1/Prac4.java
+++ StackP1/Prac4.java
@@ -1,5 +1,37 @@
 package Java_Learning.StackP1;
 
+//push at the bottom of the stack
+
+import java.util.Stack;
+
 public class Prac4 {
-    
+
+    // func to push at the bottom
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        pushAtBottom(s, 0);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+
+    }
+
 }
