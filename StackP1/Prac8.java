3c8b6fd36a98d3df0fdc1e06153e5975e44b7255
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-04-02 22:04:46 +0530

    Prac8.java modified

--- StackP1/Prac8.java
+++ StackP1/Prac8.java
@@ -1,7 +1,46 @@
 package Java_Learning.StackP1;
 
+import java.util.Stack;
+
 //Stock Span Problem
 
 public class Prac8 {
+    public static void stockSpan(int stocks[], int span[]) {
+        Stack<Integer> s = new Stack<>();
+
+        span[0] = 1;
+        s.push(0);
+
+        int currPrice;
+        for(int i = 1; i < span.length; i++){
+            currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice > stocks[s.peek()]) {
+                s.pop();
+            }
+
+            if(s.isEmpty()){
+                span[i] = i+1;
+
+            }else{
+                int prevHigh = s.peek();
+                span[i] = i- prevHigh;
+            }
+
+            s.push(i);
+        }
+    }
+
+    // main func
+    public static void main(String[] args) {
+        int stocks[] = { 100, 80, 60, 70, 60, 85, 100 };
+
+        int span[] = new int[stocks.length];
+
+        stockSpan(stocks, span);
+
+        for (int i = 0; i < span.length; i++) {
+            System.out.print(span[i] + " ");
+        }
+    }
 
 }
