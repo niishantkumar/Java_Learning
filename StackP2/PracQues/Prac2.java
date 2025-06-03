671c2ca0cf314b8e2b9858010345a747dfcbf108
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-03 22:17:51 +0530

    Prac2.java modified

--- StackP2/PracQues/Prac2.java
+++ StackP2/PracQues/Prac2.java
@@ -2,55 +2,85 @@ package Java_Learning.StackP2.PracQues;
 
 import java.util.Stack;
 
-/*
-    Decode a string
-    We have an encoded string and the task is to decode it.The pattern in which the string are encoded is as follows.
-    
-    Sample Input 1: 2[cv]
-    Sample Output 1: cvcv
-    
-    Sample Input 2: 3[b2[v]]
-    Sample Output 2: bvvbvvbvv
- */
+//decoding String problem
 
-public class Prac2 {
-    // func to decode String
-    public static String decodeString(String str) {
-        Stack<Character> stk = new Stack<>();
-        String temp;
-        String newStr = "";
+class Solution {
+    static String decode(String str) {
+        Stack<Integer> integerstack = new Stack<>();
+        Stack<Character> stringstack = new Stack<>();
+
+        String temp = "";
+        String result = "";
 
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) == ']') {
-                temp = "";
-                while (!stk.isEmpty() && stk.peek() != '[') {
-                    temp += stk.pop();
-                }
-                newStr = temp + newStr;
-                if (!stk.isEmpty() && stk.peek() == '[') {
-                    stk.pop();
-                }
+            int count = 0;
 
-                if (!stk.isEmpty() && Character.isDigit(stk.peek())) {
-
-                    int n = Character.getNumericValue(stk.pop());
-
-                    newStr = newStr.repeat(n);
+            // Step 1: If digit, calculate the full number (can be multi-digit)
+            if (Character.isDigit(str.charAt(i))) {
+                while (Character.isDigit(str.charAt(i))) {
+                    count = count * 10 + (str.charAt(i) - '0');
+                    i++;
                 }
-            } else {
-                stk.push(str.charAt(i));
+                i--; // Adjust because for-loop will also increment
+                integerstack.push(count);
             }
 
+            // Step 2: If closing bracket, decode the current pattern
+            else if (str.charAt(i) == ']') {
+                temp = "";
+                count = 0;
+
+                if (!integerstack.isEmpty()) {
+                    count = integerstack.pop();
+                }
+
+                while (!stringstack.isEmpty() && stringstack.peek() != '[') {
+                    temp = stringstack.pop() + temp;
+                }
+
+                if (!stringstack.isEmpty() && stringstack.peek() == '[') {
+                    stringstack.pop(); // Remove the opening bracket
+                }
+
+                // Repeat temp `count` times and push back to stringstack
+                result = "";
+                for (int j = 0; j < count; j++) {
+                    result += temp;
+                }
+
+                for (int j = 0; j < result.length(); j++) {
+                    stringstack.push(result.charAt(j));
+                }
+            }
+
+            // Step 3: If opening bracket
+            else if (str.charAt(i) == '[') {
+                if (i > 0 && Character.isDigit(str.charAt(i - 1))) {
+                    stringstack.push(str.charAt(i)); // Bracket after number
+                } else {
+                    // Bracket without number means repeat once
+                    stringstack.push(str.charAt(i));
+                    integerstack.push(1);
+                }
+            }
+
+            // Step 4: If character, just push it to the stringstack
+            else {
+                stringstack.push(str.charAt(i));
+            }
         }
 
-        return newStr;
+        // Step 5: Build final result from the stack
+        result = "";
+        while (!stringstack.isEmpty()) {
+            result = stringstack.pop() + result;
+        }
+
+        return result;
     }
 
-    // main func
     public static void main(String[] args) {
-        String str = "3[b2[v]]";
-
-        System.out.println(decodeString(str));
+        String str = "3[b2[ca]]";
+        System.out.println(decode(str)); // Output: bcacabcacabcaca
     }
-
 }
