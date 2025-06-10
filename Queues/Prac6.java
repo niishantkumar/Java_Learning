f80c6ac8f11f6d78c5b9a016318f8343da035f0b
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-10 23:49:29 +0530

    Prac6.java modified

--- Queues/Prac6.java
+++ Queues/Prac6.java
@@ -2,6 +2,64 @@ package Java_Learning.Queues;
 
 //Stack using 2 Ques
 
-public class Prac6 {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class Prac6 {
+    static class Stack {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty() {
+            return q1.isEmpty();
+        }
+
+        // add
+        public static void push(int data) {
+            while (!q1.isEmpty()) {
+                q2.add(q1.remove());
+            }
+
+            q1.add(data);
+
+            while (!q2.isEmpty()) {
+                q1.add(q2.remove());
+            }
+        }
+
+        // remove
+        public static int pop() {
+            if (q1.isEmpty()) {
+                System.out.println("Stack empty");
+                return -1;
+            }
+            return q1.remove();
+        }
+
+        // peek
+        public static int peek() {
+            if (q1.isEmpty()) {
+                System.out.println("Stack empty");
+                return -1;
+            }
+
+            return q1.peek();
+        }
+    }
+
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        Stack stk = new Stack();
+
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        stk.push(4);
+        stk.push(5);
+
+        while (!stk.isEmpty()) {
+            System.out.println(stk.pop());
+        }
+
+    }
 }
