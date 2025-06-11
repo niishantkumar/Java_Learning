3752d084fdb5baf52ceef36a3507698fe64d5acc
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-11 18:07:59 +0530

    Prac7.java added

--- Queues/Prac7.java
+++ Queues/Prac7.java
@@ -1,9 +1,94 @@
 package Java_Learning.Queues;
 
-//Stack using 2 Ques
+import java.util.LinkedList;
+
+//Stack using 2 Queues
 
 import java.util.Queue;
 
 public class Prac7 {
+    static class Stack {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        // isEmpty
+        public static boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        // add
+        public static void add(int data) {
+            if (q1.isEmpty()) {
+                q2.add(data);
+            } else {
+                q1.add(data);
+            }
+        }
+
+        // remove
+        public static int pop() {
+            int top = -1;
+
+            if (q2.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+
+            return top;
+        }
+
+        // peek
+        public static int peek() {
+            int top = -1;
+            if (isEmpty()) {
+                System.out.println("Stack empty");
+                return top;
+            }
+
+            if (q2.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            } else {
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+
+                    q1.add(top);
+                }
+            }
+
+            return top;
+        }
+    }
+
+    // main func
+    @SuppressWarnings("static-access")
+    public static void main(String[] args) {
+        Stack stk1 = new Stack();
+
+        stk1.add(1);
+        stk1.add(2);
+        stk1.add(3);
+        stk1.add(4);
+
+        while (!stk1.isEmpty()) {
+            System.out.println(stk1.peek());
+            stk1.pop();
+        }
+    }
 
 }
