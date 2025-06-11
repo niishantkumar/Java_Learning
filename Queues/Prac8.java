20742b85cd88bc2501a78076240bc5d29af97dcc
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-11 22:22:46 +0530

    Prac8.java modified

--- Queues/Prac8.java
+++ Queues/Prac8.java
@@ -1,7 +1,42 @@
 package Java_Learning.Queues;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 //first non-repeating letter
 
 public class Prac8 {
+
+    // func to print non-repeating
+    public static void printNonRepeating(String str) {
+        int map[] = new int[26]; // map to trace frequency of characters in string
+
+        Queue<Character> que = new LinkedList<>();
+
+        char current;
+
+        for (int i = 0; i < str.length(); i++) {
+            current = str.charAt(i);
+
+            que.add(current);
+            map[current - 'a']++;
+
+            while (!que.isEmpty() && map[que.peek() - 'a'] > 1) {
+                que.remove();
+            }
+
+            if (que.isEmpty()) {
+                System.out.print("-1 ");
+            } else {
+                System.out.print(que.peek() + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "aabccxb";
+
+        printNonRepeating(str);
+    }
 
 }
