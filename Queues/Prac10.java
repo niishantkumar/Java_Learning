e877c7d407620de7a97f54568486c4322cf61a5e
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-12 13:27:53 +0530

    Prac10.java added

---
+++ Queues/Prac10.java
@@ -0,0 +1,15 @@
+package Java_Learning.Queues;
+
+//reverse a Queue
+
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Prac10 {
+
+    // main func
+    public static void main(String[] args) {
+      
+    }
+}
