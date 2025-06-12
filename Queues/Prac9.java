e4dd499b0f652490f58c26bfdac046c40d384e3e
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-12 12:05:08 +0530

    Prac9.java modified

--- Queues/Prac9.java
+++ Queues/Prac9.java
@@ -6,8 +6,41 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class Prac9 {
+    // func to print Interleave
+    public static void printInterleave(Queue<Integer> q) {
+        if (q.size() % 2 != 0) {
+            System.out.println("Queue needs to have even number of elements");
+            return;
+        }
+
+        Queue<Integer> firstHalf = new LinkedList<>();
+
+        int size = q.size() / 2;
+        for (int i = 0; i < size; i++) {
+            firstHalf.add(q.remove());
+        }
+
+        while (!firstHalf.isEmpty()) {
+            q.add(firstHalf.remove());
+            q.add(q.remove());
+        }
+
+        // loop to print
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
+    }
+
     // main func
     public static void main(String[] args) {
+        Queue<Integer> q1 = new LinkedList<>();
+        q1.add(1);
+        q1.add(2);
+        q1.add(3);
+        q1.add(4);
+        q1.add(5);
+        q1.add(6);
 
+        printInterleave(q1);
     }
 }
