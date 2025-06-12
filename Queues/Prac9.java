5a2d9102656e306e237aa46d2b42e2a70b75024d
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-12 11:36:23 +0530

    Prac9.java

---
+++ Queues/Prac9.java
@@ -0,0 +1,13 @@
+package Java_Learning.Queues;
+
+//Interleave two halves of a Queue
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Prac9 {
+    // main func
+    public static void main(String[] args) {
+
+    }
+}
