a143e28e6f3934d480389f5abacd98d393e8fa0b
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-13 17:02:46 +0530

    Prac11.java modified

--- Queues/Prac11.java
+++ Queues/Prac11.java
@@ -2,6 +2,31 @@ package Java_Learning.Queues;
 
 //implementing Deque using JCF
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class Prac11 {
+
+    public static void main(String[] args) {
+        Deque<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.addFirst(2);
+        q.addFirst(3);
+        q.addLast(4);
+
+        for (Integer i : q) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        q.remove();
+        q.removeFirst();
+        q.removeLast();
+
+        for (Integer i : q) {
+            System.out.print(i + " ");
+        }
+    }
 
 }
