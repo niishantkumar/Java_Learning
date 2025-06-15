638c5a5b7d6f715e2fdabf8008670d9f4d32d1e2
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-15 23:33:55 +0530

    Prac2.java added

---
+++ Queues/PracQues/Prac2.java
@@ -0,0 +1,18 @@
+package Java_Learning.Queues.PracQues;
+
+/*
+ ************Connect n ropes with minimum cost**************
+
+ Given are N ropes of different lengths, the task is to connect these ropes into one rope with
+ minimum cost, such that the cost to connect two ropes is equal to the sum of their lengths.
+
+ Sample Input 1 : N = 4, arr = [4 3 2 6]
+ Sample Output 1 : 29
+ 
+ Sample Input 2 : N = 2, arr = [1 2 3]
+ Sample Output 2 : 9
+ */
+
+public class Prac2 {
+
+}
