9634ffb630f8939bf8abf62fb8a2055dae8c5611
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-15 20:21:35 +0530

    Prac1.java modified

--- Queues/PracQues/Prac1.java
+++ Queues/PracQues/Prac1.java
@@ -10,6 +10,28 @@ package Java_Learning.Queues.PracQues;
     Sample Output 2 : 1 10 11 100 101
 */
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Prac1 {
+    // func to print binary
+    public static void printBinary(int num) {
+        Queue<String> q = new LinkedList<>();
+        q.add("1");
+
+        for (int i = 0; i < num; i++) {
+            String curr = q.remove();
+            System.out.print(curr + " ");
+            q.add(curr + "0");
+            q.add(curr + "1");
+        }
+
+    }
+
+    // main func
+    public static void main(String[] args) {
+        int num = 5;
+        printBinary(num);
+    }
 
 }
