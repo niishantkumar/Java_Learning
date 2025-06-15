36b7659b40bd32bf9a78d0e51f3a5294c86282bc
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-15 17:38:10 +0530

    Prac1.java added

---
+++ Queues/PracQues/Prac1.java
@@ -0,0 +1,15 @@
+package Java_Learning.Queues.PracQues;
+
+/*  
+    Given a number N. The task is to generate and print all binary numbers with decimal values from
+    1 to N.
+    Sample Input 1 : N =2
+    Sample Output 1 : 1 10
+    
+    Sample Input 2 : 5.
+    Sample Output 2 : 1 10 11 100 101
+*/
+
+public class Prac1 {
+
+}
