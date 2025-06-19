4db4fdd63e99c48c6a4ba9eb3a381a14fd685fa7
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-19 15:12:46 +0530

    Prac4.java added

---
+++ Queues/PracQues/Prac4.java
@@ -0,0 +1,14 @@
+package Java_Learning.Queues.PracQues;
+
+/*
+ *********Reversing the first K elements of a Queue************
+
+ We have an integer k and a queue of integers, we need to reverse the order of the first k
+ elements of the queue, leaving the other elements in the same relative order.
+ Sample Input 1 : Q = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100] ,k=5
+ Sample Output 1 : Q = [50, 40, 30, 20, 10, 60, 70, 80, 90, 100]
+ */
+
+public class Prac4 {
+ 
+}
