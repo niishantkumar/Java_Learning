83b93e13ee577733a6ab60687e958cde68ae3396
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-19 18:26:43 +0530

    Prac3.java modified

--- Queues/PracQues/Prac5.java
+++ Queues/PracQues/Prac5.java
@@ -1,5 +1,9 @@
 package Java_Learning.Queues.PracQues;
 
+import java.util.Collections;
+import java.util.Queue;
+import java.util.LinkedList;
+
 /*
  ********Maximum of all subarrays of size k********
  
@@ -10,5 +14,31 @@ package Java_Learning.Queues.PracQues;
  */
 
 public class Prac5 {
+    // max of each contiguous aarray
+    public static void max_in_subArray(int arr[], int k) {
+        Queue<Integer> q = new LinkedList<>();
 
+        for (int i = 0; i < k; i++) {
+            q.add(arr[i]);
+        }
+        System.out.print(Collections.max(q) + " ");
+
+        for (int i = k; i < arr.length; i++) {
+            q.remove();
+            q.add(arr[i]);
+            System.out.print(Collections.max(q) + " ");
+
+        }
+
+    }
+
+    // main func
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+
+        int k = 3;
+
+        max_in_subArray(arr, k);
+
+    }
 }
