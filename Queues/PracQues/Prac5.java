355e11c219ff1b9e6a3655a70d7d3a7d3ef1c349
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-19 15:20:51 +0530

    Prac5.java

---
+++ Queues/PracQues/Prac5.java
@@ -0,0 +1,14 @@
+package Java_Learning.Queues.PracQues;
+
+/*
+ ********Maximum of all subarrays of size k********
+ 
+ We have an array arr[] of size N and an integer K. Find the maximum for each and every
+ contiguous subarray of size K.
+ Sample Input 1 : N=9, K=3 arr= 1 2 3 1 4 5 2 3 6
+ Sample Output 1 : 3 3 4 5 5 5 6
+ */
+
+public class Prac5 {
+
+}
