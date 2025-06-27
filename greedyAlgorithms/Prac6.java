b9c962d25589bf5010a77141434d553d6b77ae09
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-06-27 21:38:17 +0530

    Prac6.java modified

--- greedyAlgorithms/Prac6.java
+++ greedyAlgorithms/Prac6.java
@@ -1,11 +1,38 @@
 package Java_Learning.greedyAlgorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
 //Indian coins
 
 public class Prac6 {
 
     public static void main(String[] args) {
 
+        Integer coins[] = { 1, 2, 5, 10, 20, 50, 100, 500, 1000 };
+        int amount = 590;
+
+        Arrays.sort(coins, Comparator.reverseOrder());
+
+        int countOfcoins = 0;
+        ArrayList<Integer> coinsUsed = new ArrayList<>();
+
+        for (int i = 0; i < coins.length && amount > 0; i++) {
+
+            if (coins[i] <= amount) {
+                while (coins[i] <= amount) {
+                    amount -= coins[i];
+                    countOfcoins++;
+                    coinsUsed.add(coins[i]);
+                }
+
+            }
+        }
+
+        System.out.println("Number of coins/notes used : " + countOfcoins);
+
+        System.out.println("Coins/notes used : " + coinsUsed);
     }
 
 }
