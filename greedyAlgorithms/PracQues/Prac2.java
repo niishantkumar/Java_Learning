c791ea20a4773e78fd32e8208ab6b9ebe8e88938
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-07-02 23:13:27 +0530

    Prac2.java modified

--- greedyAlgorithms/PracQues/Prac2.java
+++ greedyAlgorithms/PracQues/Prac2.java
@@ -8,9 +8,23 @@ package Java_Learning.greedyAlgorithms.PracQues;
 
  Sample Input 1 : L = -3, R = 3, K = 1
  Sample Output 1 : 3
- 
+
  */
 
 public class Prac2 {
+    // func to find Kth largest odd number in a given range
+    public static int Kth_largOdd(int l, int r, int k) {
+        int Kth_largOdd = r - (2 * (k - 1));
+
+        return Kth_largOdd < l ? 0 : Kth_largOdd;
+    }
+
+    public static void main(String[] args) {
+        int l = -3;
+        int r = 3;
+        int k = 1;
+
+        System.out.println(Kth_largOdd(l, r, k));
+    }
 
 }
