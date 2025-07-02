5a15c821fcd6424decccb5dd9b1db86c4b9294b8
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-07-02 22:53:05 +0530

    Prac2.java added

---
+++ greedyAlgorithms/PracQues/Prac2.java
@@ -0,0 +1,16 @@
+package Java_Learning.greedyAlgorithms.PracQues;
+
+/*
+ *  *******Kth largest odd number in a given range*********
+ We have two variables L and R, indicating a range of integers from L to R inclusive, and a
+ number K, the task is to find Kth largest odd number. If K > number of odd numbers in the range
+ L to R then return 0.
+
+ Sample Input 1 : L = -3, R = 3, K = 1
+ Sample Output 1 : 3
+ 
+ */
+
+public class Prac2 {
+
+}
