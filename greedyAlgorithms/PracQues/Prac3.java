d481ff492fca8fe8eef13dd5959f2ede7d44bda7
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-07-03 23:10:12 +0530

    Prac3.java added

---
+++ greedyAlgorithms/PracQues/Prac3.java
@@ -0,0 +1,25 @@
+package Java_Learning.greedyAlgorithms.PracQues;
+
+/*
+
+ *******Lexicographically smallest string of length N and sum K********
+
+ We have two integers N and K. The task is to print the lexicographically smallest string of
+ length N consisting of lower-case English alphabets such that the sum of the characters of
+ the string equals to K where ‘a’ = 1, ‘b’ = 2, ‘c’ = 3, ….. and ‘z’ = 26.
+
+ Sample Input 1 : N = 5, K = 42
+ Sample Output 1 : aamz
+ 
+ Sample Input 2 : N = 3, K = 25
+ Sample Output 2 : aaw
+
+*/
+
+public class Prac3 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
