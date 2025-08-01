b5f6cc08b894bd474530d6e4d86c63b9291420df
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-08-01 21:12:55 +0530

    Prac13.java added

---
+++ BinaryTreesP1/Prac13.java
@@ -0,0 +1,23 @@
+package Java_Learning.BinaryTreesP1;
+
+//lowest common ancestor (Approach 2)
+
+public class Prac13 {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
