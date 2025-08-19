bb63f5789d393dcda381908ab8081e504150928a
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-08-20 00:25:05 +0530

    Prac1.java added

---
+++ BinaryTreesP1/PracQues/Prac1.java
@@ -0,0 +1,23 @@
+package Java_Learning.BinaryTreesP1.PracQues;
+
+/* 
+ * Check if a Binary Tree is univalued or not 
+ * We have a binary tree, the task is to check if the binary tree is univalued or not.
+ * If found to be true, then print “YES”. Otherwise, print “NO”
+ */
+
+public class Prac1 {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+
+}
