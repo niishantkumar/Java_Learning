6ea49ea0da9e2acfbf271be1ae5a6d5ddd21e2d0
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-08-20 00:46:55 +0530

    Prac1.java modified

--- BinaryTreesP1/PracQues/Prac1.java
+++ BinaryTreesP1/PracQues/Prac1.java
@@ -20,4 +20,48 @@ public class Prac1 {
 
     }
 
+    //helper func
+    static boolean isUnivalTree(Node root) {
+        if (root == null)
+            return true;
+        return isUnivalued(root, root.data);
+    }
+
+    //func to check univalued
+    private static boolean isUnivalued(Node root, int data) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.data != data) {
+            return false;
+        }
+
+        boolean leftCheck = isUnivalued(root.left, data);
+        boolean rightCheck = isUnivalued(root.right, data);
+
+        return leftCheck && rightCheck;
+    }
+
+    public static void main(String[] args) {
+        /*
+         *    2
+         *   / \
+         *  2   2
+         * / \ / \
+         * 5 2 2  2
+         * 
+         */
+
+         Node root = new Node(2);
+         root.left = new Node(2);
+         root.right = new Node(2);
+         root.left.left =new Node(5);
+         root.left.right = new Node(2);
+         root.right.left =new Node(2);
+         root.right.right =new Node(2);
+
+         isUnivalTree(root);
+    }
+
 }
