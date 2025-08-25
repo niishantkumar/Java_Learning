092127d44c1169478d23cdcacc6266fa563c3187
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-08-25 19:37:37 +0530

    Prac2.java added

--- BinarySearchTrees/Prac2.java
+++ BinarySearchTrees/Prac2.java
@@ -3,5 +3,80 @@ package Java_Learning.BinarySearchTrees;
 //search in a BST
 
 public class Prac2 {
-    
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data > key) {
+            return search(root.left, key); // if found in left
+        } else {
+            return search(root.right, key); // if found in right
+        }
+    }
+
+    // func to build BST
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int values[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inorder(root);
+        System.out.println();
+
+        int key = 3;
+        if (search(root, key)) {
+            System.out.println(key + " found");
+        } else {
+            System.out.println(key + " not found");
+        }
+
+    }
+
 }
