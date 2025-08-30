d00a394e850bd9e72211ed0c108ac0a9f8b00cdf
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-08-30 20:53:23 +0530

    Prac7.java modified

--- BinarySearchTrees/Prac7.java
+++ BinarySearchTrees/Prac7.java
@@ -5,4 +5,75 @@ package Java_Learning.BinarySearchTrees;
 
 public class Prac7 {
 
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // func to check if BST is valid or not
+    public static boolean isValid(Node root, Node min, Node max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (min != null && root.data <= min.data) {
+            return false;
+        }
+
+        if (max != null && root.data >= max.data) {
+            return false;
+        }
+
+        return isValid(root.left, min, root) && isValid(root.right, root, max);
+    }
+
+    // func to build BST
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int values[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        if (isValid(root, null, null)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Not valid");
+        }
+    }
 }
