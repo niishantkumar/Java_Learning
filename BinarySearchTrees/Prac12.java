536e30ca9a748294dc04e6d9fe101662bf798212
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-09-02 20:48:05 +0530

    Prac12.java added

---
+++ BinarySearchTrees/Prac12.java
@@ -0,0 +1,23 @@
+package Java_Learning.BinarySearchTrees;
+
+//merge two BSTs
+
+public class Prac12 {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
