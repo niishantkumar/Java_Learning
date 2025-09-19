3a94bf5bb0286181644c1ff7ff116f798bc48932
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-09-19 20:49:42 +0530

    Prac1.java added

---
+++ BinarySearchTrees/PracQues/Prac1.java
@@ -0,0 +1,11 @@
+package Java_Learning.BinarySearchTrees.PracQues;
+
+/*
+------Range Sum of BST------
+ * We have a Binary Search Tree consisting of N nodes and two positive integers L and R,
+ * the task is to find the sum of values of all the nodes that lie in the range [L, R]..
+ */
+
+public class Prac1 {
+
+}
