4a31b98aa613dfac586b63825016ba8728421a06
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-09-20 01:03:37 +0530

    Prac2.java added

---
+++ BinarySearchTrees/PracQues/Prac2.java
@@ -0,0 +1,11 @@
+package Java_Learning.BinarySearchTrees.PracQues;
+
+/*
+----------Find the closest element in Binary Search Tree-----------
+ * We have a binarysearch tree and a target node K.
+ * The task is to find the node with minimum absolute difference with given target value K.
+ */
+
+public class Prac2 {
+
+}
