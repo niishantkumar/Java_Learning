cb8576fe8a0a3449ad86ee68ab3b90f6e6d67558
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-09-20 23:58:22 +0530

    Prac1 & Prac2 updated

--- BinarySearchTrees/PracQues/Prac1.java
+++ BinarySearchTrees/PracQues/Prac1.java
@@ -8,4 +8,88 @@ package Java_Learning.BinarySearchTrees.PracQues;
 
 public class Prac1 {
 
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // func to find sum of nodes having val in range [L,R]
+
+    public static int sumInRange(Node root, int l, int r) {
+        if (root == null) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        // If root is in range, add it
+        if (root.data >= l && root.data <= r) {
+            sum += root.data;
+        }
+
+        // If root is greater than L, left subtree might have nodes in range
+        if (root.data > l) {
+            sum += sumInRange(root.left, l, r);
+        }
+
+        // If root is less than R, right subtree might have nodes in range
+        if (root.data < r) {
+            sum += sumInRange(root.right, l, r);
+        }
+
+        return sum;
+    }
+
+    // func to build BST
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+
+        if (root.data > val) {
+            // left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int values[] = { 5, 3, 6, 8, 11, 20 };
+        Node root = null;
+
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        inorder(root);
+
+        System.out.println();
+
+        int l = 5;
+        int r = 11;
+
+        System.out.println("Sum in range " + l + "," + r + " = " + sumInRange(root, 5, 11));
+    }
 }
--- BinarySearchTrees/PracQues/Prac2.java
+++ BinarySearchTrees/PracQues/Prac2.java
@@ -2,10 +2,105 @@ package Java_Learning.BinarySearchTrees.PracQues;
 
 /*
 ----------Find the closest element in Binary Search Tree-----------
- * We have a binarysearch tree and a target node K.
- * The task is to find the node with minimum absolute difference with given target value K.
+ * Given a Binary Search Tree and a target value K,
+ * Find the node with the minimum absolute difference from K.
  */
 
 public class Prac2 {
 
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // To keep track of the closest node found so far
+    static Node minDiffNode = null;
+
+    // Function to find the node with the minimum absolute difference to K
+    public static int minAbsDiff(Node root, int k) {
+        Node curr = root;
+        int minDiff = Integer.MAX_VALUE;
+
+        while (curr != null) {
+            int currDiff = Math.abs(curr.data - k);
+
+            if (currDiff < minDiff) {
+                minDiff = currDiff;
+                minDiffNode = curr;
+            }
+
+            // Move left or right depending on the value of K
+            if (k < curr.data) {
+                curr = curr.left;
+            } else if (k > curr.data) {
+                curr = curr.right;
+            } else {
+                // Exact match found
+                break;
+            }
+        }
+
+        return minDiff;
+    }
+
+    // Insert a new node into the BST
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    // Inorder traversal to print BST
+    public static void inorder(Node root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int[] values = { 5, 3, 6, 8, 11, 20 };
+        Node root = null;
+
+        // Build the BST
+        for (int val : values) {
+            root = insert(root, val);
+        }
+
+        // Print inorder traversal
+        System.out.print("Inorder traversal: ");
+        inorder(root);
+        System.out.println();
+
+        // Target value
+        int k = 10;
+
+        // Reset minDiffNode before finding closest
+        minDiffNode = null;
+
+        int closestDiff = minAbsDiff(root, k);
+
+        if (minDiffNode != null) {
+            System.out.println("Closest value to " + k + " is: " + minDiffNode.data);
+            System.out.println("Minimum absolute difference: " + closestDiff);
+        } else {
+            System.out.println("Tree is empty.");
+        }
+    }
 }
