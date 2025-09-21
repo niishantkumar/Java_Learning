bd54dbcc911b742ed88a7b50d803d0156a2bb2b5
Nishant Kumar <nishant.kumar2839@gmail.com>
2025-09-22 01:09:00 +0530

    Prac3.java added

---
+++ BinarySearchTrees/PracQues/Prac3.java
@@ -0,0 +1,10 @@
+package Java_Learning.BinarySearchTrees.PracQues;
+
+/*
+ * ------------Find k-th smallest element in BST---------------
+ * We have the root of a binary search tree and K as input, find Kth smallest element in BST.
+ */
+
+public class Prac3 {
+
+}
